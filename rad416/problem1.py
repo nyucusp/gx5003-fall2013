a8490f24611caa9c10b2cc4d9638e9ac26f91789
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-13 15:26:06 -0400

    take in command line arguments

--- rad416/problem1.py
+++ rad416/problem1.py
@@ -1,48 +1,36 @@
-# input any two numbers between 0 and 1,000,000, 
-# Given any two numbers i and j, you are to determine the maximum cycle length over all numbers between i and j, including both endpoints.
-# Input
-
-# The input will consist of a series of pairs of integers i and j, one pair of integers per line. All integers will be less than 1,000,000 and greater than 0.
-# Output
-
-# For each pair of input integers i and j, output i, j in the same order in which they appeared in the input and then the maximum cycle length for integers between and including i and j. These three numbers should be separated by one space, with all three numbers on one line and with one line of output for each line of input.
+import sys
 
 #initialize and declare variables
-# int maxCycleLength = 0
-cycleCount = 1
-n = 22
+maxCycleLength = 2
 
-def getNextNumber(n):
+def getNextNumber(n): #generates sequence
   if (n%2 == 0): #n is even
     return (n / 2)
   else: #n is odd
     return (3 * n + 1)
 
 
-#take in start and end
-while n != 1:
-  n=getNextNumber(n)
-  cycleCount += 1
+for i in range(len(sys.argv)):
+  if i==1:
+    rangeStart = int(sys.argv[i])
+  if i==2:
+    rangeEnd = int(sys.argv[i])
+  if i==3:
+      break
 
-print(cycleCount)
-#generate sequence
-  #test even
-  #test odd
-#count sequence
-  #return cycle length
 
-# def argparse(argv):
-#     for i in range(len(argv)):
-#         if i==1:
-#             if(vetdeploymentname(argv[i]))==argv[i]:
-#                 deploymentnamelist=[argv[i]]
-#         if i==2:
-#             if(vetdate(argv[i]))==argv[i]:
-#                 startdate=argv[i]
-#         if i==3:
-#             if(vetdate(argv[i]))==argv[i]:
-#                 enddate=argv[i]
-#         if i>3:
-#             break
+for i in range(rangeStart, rangeEnd):
+  cycleLength = 1
+  #take in start and end
+  while i != 1:
+    i = getNextNumber(i)
+    cycleLength += 1
+
+  #test cycleLength against maxCycleLength, replace if greater
+  if (cycleLength > maxCycleLength):
+    maxCycleLength = cycleLength
+
+print rangeStart, rangeEnd, maxCycleLength
+
 
 
