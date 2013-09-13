12c64f966d31ded43d2c568eb2993822775c4603
Warren Reed <Warren@new-host-2.home>
2013-09-13 19:19:50 -0400

    main method

--- jwr300/Assignment1/Problem1.py
+++ jwr300/Assignment1/Problem1.py
@@ -2,18 +2,27 @@
 
 import sys
 
-begin = int(sys.argv[1])
-end = int(sys.argv[2])
-tracker = end
-counter = 1 #set to 1 to include endpoints
+def main(argv):
 
-while tracker > begin:
-    #if tracker is even, divide by 2
-    if tracker%2 == 0:
-        tracker = tracker/2
-        counter+=1
-    elif tracker%2 == 1:
-        tracker = 3*tracker + 1
-        counter+=1
-        
-print str(begin) + " " + str(end) + " " + str(counter)
+    begin = int(sys.argv[1])
+    end = int(sys.argv[2])
+    tracker = end
+    counter = 1 #set to 1 to include endpoints
+
+    while tracker > begin:
+        #if tracker is even, divide by 2
+        if tracker%2 == 0:
+            tracker = tracker/2
+            counter+=1
+        elif tracker%2 == 1:
+            tracker = 3*tracker + 1
+            counter+=1
+            
+    print str(begin) + " " + str(end) + " " + str(counter)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
+
