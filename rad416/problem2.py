19288eaf9ffb74e30d7bb57adaae2ecd102f4940
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-13 17:37:31 -0400

    return max value of argument string

--- rad416/problem2.py
+++ rad416/problem2.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 # A sequence of n > 0 integers is called a jolly jumper if the absolute values of the differences between successive elements take on all possible values 1 through n - 1. For instance,
 
@@ -14,6 +15,24 @@ import sys
 
 # For each line of input generate a line of output saying ``Jolly'' or ``Not jolly''.
 
-print "Jolly"
+#generate 1 through n - 1
 
-print "Not jolly"
+maxValue = 1
+
+for i in range(1,len(sys.argv)):
+  if (sys.argv[i] > maxValue):
+    maxValue = sys.argv[i]
+
+
+print maxValue
+
+# #Take in integer string
+# for i in range(len(sys.argv)):
+#   if (fabs(int(sys.argv[i]) - int(sys.argv[i+1]) > )
+
+# #calc and test successive absolute differences
+
+
+# print "Jolly"
+
+# print "Not jolly"
