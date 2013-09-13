9b8b3a08a0df1ed8f1c34ef01e53d0de910f6415
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-13 15:34:09 -0400

    cleaned up text and removed extraneous comments

--- rad416/problem1.py
+++ rad416/problem1.py
@@ -1,23 +1,20 @@
 import sys
 
-#initialize and declare variables
 maxCycleLength = 2
 
 def getNextNumber(n): #generates sequence
-  if (n%2 == 0): #n is even
+  if (n % 2 == 0): #n is even
     return (n / 2)
   else: #n is odd
     return (3 * n + 1)
 
-
 for i in range(len(sys.argv)):
-  if i==1:
+  if i == 1:
     rangeStart = int(sys.argv[i])
-  if i==2:
+  if i == 2:
     rangeEnd = int(sys.argv[i])
-  if i==3:
+  if i == 3:
       break
-
 
 for i in range(rangeStart, rangeEnd):
   cycleLength = 1
