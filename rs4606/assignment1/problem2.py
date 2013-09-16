5e09ba410f6a2cd8503073218c67589cd29038f4
Ravi Shroff <rs4606@nyu.edu>
2013-09-15 21:46:54 -0400

    added comments for problem1 and problem2

--- rs4606/assignment1/problem2.py
+++ rs4606/assignment1/problem2.py
@@ -7,6 +7,14 @@ length = int(input[0])
 
 count = 0
 
+"""
+In the following loop, i runs through the range from 1 to the length of the input
+integer minus 1.  We compare it to each difference in the sequence in turn in 
+the 'if' statement.  We use the dummy variable s to exit the while loop, and 
+we increment the variable count to see how many differences match with our
+variable i.
+"""
+
 for i in range(1,length):
     s = 0
     while s == 0:
