1785279971331eb1b2825315d9b8de9c6ad55fe5
Abdullah Kurkcu <ak4728@nyu.edu>
2013-09-16 15:46:56 -0400

    Version 2

--- ak4728/problem1.py
+++ ak4728/problem1.py
@@ -1,29 +1,28 @@
-def memoized(fn):
-        memoized_lengths = [0] * 1000000
-        def fn2(n):
-            if not memoized_lengths[n]:
-                memoized_lengths[n] = fn(n)
-            return memoized_lengths[n]
-        return fn2
-    def cycle_length(n):
-        if n == 1: return 1
-        if n % 2:
-            return cycle_length(n * 3 + 1) + 1
-        return cycle_length(n / 2) + 1
-    
-    def problem(i,j):
-        return max([cycle_length(x) for x in range(i, j+1)])
-        
-    def test_cycle_length():
-        assert cycle_length(22) == 16
-        assert cycle_length(10) == 7
-        assert cycle_length(11) == 15
-        print 'OK'
-    
-    def test_problem():
-        print 1, 10, problem(1, 10)
-        print 100, 200, problem(100, 200)
-        print 201, 210, problem(201, 210)
-        print 900, 100, problem(900, 1000)
-    
-    if __name__ == '__main__': test_problem()
+#cycle
+def cycle(nr):
+ count=0;
+ while nr!=1:
+  if nr%2==1:
+   nr=3*nr+1
+  else:
+   nr=nr/2
+  count=count+1
+ count=count+1
+ return count
+#interval
+def interval(i,j):
+ n=0
+ max=cycle(i)
+ for x in range (i+1,j):
+  n=cycle(x)
+  if max<=n:
+   max=n
+ print(i,j,max)
+#main
+sw=1
+while sw==1:
+ i=int(input())
+ j=int(input())
+ interval(i,j)
+ print("Continue? 1=Yes 0=No")
+ sw=int(input())
