7ac2cf457e4a5457a872d91482ad09c4f757d2f1
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-16 20:56:08 -0400

    added function to count mines, fixing logic issue for out of bounds and self-check

--- rad416/problem3.py
+++ rad416/problem3.py
@@ -6,16 +6,48 @@ fieldCols = 3
 
 field = [[".",".","*"],["*",".","."],["*",".","."]]
 
-#write matrix
-#def writeMatrix(rows, cols):
 
-def displayMatrix(matrix):
-  for i in range(len(matrix[0])):
-    for j in range(len(matrix)):
-      print matrix[i][j],
+
+
+def displayfield(field):
+  for i in range(len(field[0])):
+    for j in range(len(field)):
+      print field[i][j],
     print "\n"
 
-displayMatrix(field)
+def countMines(field):
+  mineCountArray = []
+  for i in range(len(field[0])):
+    print "row iteration"
+    for j in range(len(field)):
+      # mineCountArray[i][j] = 
+      calcMineSum(field,i,j)
+  # return mineCountArray
+
+  #for each position in field
+def calcMineSum(field, row, col):
+  mineSum = 0
+  rowMax = len(field[0])
+  colMax = len(field)
+  print "rowMax is ", rowMax
+  print "colMax is ", colMax
+  print "row is ", row
+  print "col is ", col
+
+  for i in range(row-1,row+2):
+    for j in range(col-1, col+2):
+      print "this is ",i,j
+      if (i < 0 or j < 0 or i >= rowMax or j >= rowMax or (i == row and j == col)):
+        print "breaking..."
+        continue
+      elif (field[i][j] == "*"):
+        mineSum += 1
+  print "The mineSum of cell", row, col, " is ", mineSum
+
+displayfield(field)
+
+# mineCount = 
+countMines(field)
+# displayfield(mineCount)
 
 
-#generate counts of mines
