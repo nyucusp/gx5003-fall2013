75af53774978485d84d8bbf8125c09279aba3e4b
Natan Seltzer <seltzn01@nyu.edu>
2013-09-18 22:20:32 -0400

    all

--- seltzn01/problem2.py
+++ seltzn01/problem2.py
@@ -1,8 +1,9 @@
-# problem2.py
+#Nathan Seltzer
+#problem2.py
+
 import sys
 i = sys.argv[1]
 j = sys.argv[2]
 
-import webbrowser
-webbrowser.open('http://www.jollyjumper.com/images/products/medium/104_OriginalJollyJumper.jpg')
+http://www.jollyjumper.com/images/products/medium/104_OriginalJollyJumper.jpg'
 
