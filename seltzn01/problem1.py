56c48bebd19f29c3957094b80b313ad22b2233c0
Natan Seltzer <seltzn01@nyu.edu>
2013-09-18 20:01:20 -0400

    problem1.py

---
+++ seltzn01/problem1.py
@@ -0,0 +1,14 @@
+#problem1.py
+import sys
+i = sys.argv[1]
+j = sys.argv[2]
+for n in range(i,j):
+	print n
+ array = [i,j]
+	if n % 2 == 0:
+		return n/2
+	else:
+		return n * 3 + 1
+			aux... # figure out
+	array.append(i)
+print 
