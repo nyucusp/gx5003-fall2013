8542b3eace3b799888a704592cd471c1c0c67096
Winnie Kaaria <wkk229@nyu.edu>
2013-09-19 23:05:07 -0400

    problem3.py

--- wkk229/problem3.py
+++ wkk229/problem3.py
@@ -1,31 +1,33 @@
 import sys
 print sys.argv
- import random
+import select
+import random
 
- LA=["x","x","x" ,"x","x","x","x"]
- LB=["x","x","x","x","x","x","x"]
- LC=["x","x","x","x","x","x","x"]
- LD=["x","x","x","x","x","x","x"]
- lE=["x","x","x","x","x","x","x"]
- LF=["x","x","x","x","x","x","x"]
- LG=["x","x","x","x","x","x","x"]
+LA=["x","x","x" ,"x","x","x","x"]
+LB=["x","x","x","x","x","x","x"]
+LC=["x","x","x","x","x","x","x"]
+LD=["x","x","x","x","x","x","x"]
+LE=["x","x","x","x","x","x","x"]
+LF=["x","x","x","x","x","x","x"]
+LG=["x","x","x","x","x","x","x"]
 
- print("", LA,"\n",LB,"\n",LC,"\n",LD,"\n",lE,"\n",LF,"\n",LG,"\n")
+print("", LA,"\n",LB,"\n",LC,"\n",LD,"\n",LE,"\n",LF,"\n",LG,"\n")
 
- print("\n" select row starting from top = 1 and column from left = 0)
+print"\n" 
+#select.row starting from top = 1 and column from left = 0
 
-num = random.randit (1,7)
-numb= random.randit(0,6)
+num = random.randint(1,7)
+numb= random.randint(0,6)
 MINE= "0"
 
 row = 9
 column = 9
 one = "1"
-blank = "-"
+zero = "-"
 
 while row != num or column != numb:
 	print("",LA ,"\n", LB, "\n", LC,"\n",LD,"\n", LE,"\n",LF, "\n", LG,"\n")
-	 )#cheeter 
+	#cheeter 
 	print(num, "" , numb)
 	row = int (input("nEnter row"))
 	column =int (input("nEnter column"))
@@ -35,7 +37,7 @@ while row != num or column != numb:
 	rowB = row + 1
 
 	if rowA == num and column == numb:
-		if row == 1
+		if row == 1:
 			del LA [column]
 			LA.instert( column ,one)
 		if row == 2:
@@ -57,7 +59,7 @@ while row != num or column != numb:
 			del LG [column]
 			LG.instert( column ,one)
 	elif row == num and columnA == numb :
-		if row == 1
+		if row == 1:
 			del LA [column]
 			LA.instert( column ,one)
 		if row == 2:
@@ -79,7 +81,7 @@ while row != num or column != numb:
 			del LG [column]
 			LG.instert( column ,one)
 	elif row == num and columnB == numb :
-		if row == 1
+		if row == 1:
 			del LA [column]
 			LA.instert( column ,one)
 		if row == 2:
@@ -101,52 +103,52 @@ while row != num or column != numb:
 			del LG [column]
 			LG.instert( column ,one)
 	else :
-		if row == 1
+		if row == 1:
 			del LA [column]
 			LA.instert( column ,zero)
 		if row == 2:
 			del LB [column]
-			LB.instert( column ,zero)
+			LB.insert(column ,zero)
 		if row == 3:
 			del LC [column]
-			LC.instert( column ,zero)
+			LC.insert( column ,zero)
 		if row == 4:
 			del LD [column]
-			LD.instert( column ,zero)
+			LD.insert( column ,zero)
 		if row == 5:
 			del LE [column]
-			LE.instert( column ,zero)
+			LE.insert( column ,zero)
 		if row == 6 :
 			del LF [column]
-			LF.instert( column ,zero)
+			LF.insert( column ,zero)
 		if row == 7:
 			del LG [column]
-			LG.instert( column ,zero)
+			LG.insert( column ,zero)
 	
 if row ==1:
 	
     del LA [column]
-	LA.instert( column ,MINE)
+LA.insert( column ,MINE)
 if row == 2:
 	del LB [column]
-	LB.instert( column ,MINE)
+	LB.insert( column ,MINE)
 if row == 3:
 	del LC [column]
-	LC.instert( column ,MINE)
+	LC.insert( column ,MINE)
 if row == 4:
 	del LD [column]
-	LD.instert( column ,MINE)
+	LD.insert( column ,MINE)
 if row == 5:
 	del LE [column]
-	LE.instert( column ,MINE)
+	LE.insert( column ,MINE)
 if row == 6 :
 	del LF [column]
-	LF.instert( column ,MINE)
+	LF.insert( column ,MINE)
 if row == 7:
 	del LG [column]
-	LG.instert( column ,MINE)	
+	LG.insert( column ,MINE)	
 
-print("",LA ,"\n", LB, "\n", LC,"\n",LD,"\n", LE,"\n",LF, "\n", LG,"\n"))
+print("",LA ,"\n", LB, "\n", LC,"\n",LD,"\n", LE,"\n",LF, "\n", LG,"\n")
 print ("Gmae over")
 
 input ("press enter to exit")
