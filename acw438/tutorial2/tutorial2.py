62ef649bbe3027dcdb28a513724e71b8f9478096
Alex Chohlas-Wood <chohlasa@gmail.com>
2013-09-22 11:01:10 -0400

    Tweaked tutorial2 file.

--- acw438/tutorial2/tutorial2.py
+++ acw438/tutorial2/tutorial2.py
@@ -4,10 +4,11 @@ from borough import Borough
 import csv
 
 bFileRaw = open('boroughs.csv', 'rU')
+bFile = csv.reader(bFileRaw, delimiter=',')
+
 boroughs = {}
 zipcode = 0
 
-bFile = csv.reader(bFileRaw, delimiter=',')
 for line in bFile:
     if line[1] not in boroughs:
         boroughs[line[1]] = Borough(line[1])
@@ -15,7 +16,8 @@ for line in bFile:
     boroughs[line[1]].addZipCode(zipcode)
 
 for key in boroughs:
-    curBor = boroughs[key].zipcodes
-    print key + ":", ", ".join(x.number for x in curBor)
+    curBorZips = boroughs[key].zipcodes
+    curBor = boroughs[key].name
+    print curBor + ":", ", ".join(x.number for x in curBorZips)
 
 bFileRaw.close()
