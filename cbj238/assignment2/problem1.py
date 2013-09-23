96cba202c0b136c674a55b3dd56da40c56fe296f
Christopher Jacoby <cbj238@nyu.edu>
2013-09-23 11:34:38 -0400

    added class for handling gitlog

--- cbj238/assignment2/problem1.py
+++ cbj238/assignment2/problem1.py
@@ -1,28 +1,12 @@
 import sys
+import gitlog
 
-collabName = sys.argv[1]
+#collabName = sys.argv[1]
 
-myFile = open('logsofar.txt', 'r')
+log = gitlog.GitLog('logsofar.txt')
 
-state = 0
-commitDateTimes = []
-for line in myFile:
-    if state == 0: # look for the author line
-        indexOfCollab = line.find(collabName)
-        if indexOfCollab != -1: # found a commit from Collab
-            state = 1
-    else:
-        indexOfDate = line.find("Date:")
-        if indexOfDate != -1: # found date from the previous commit
-            commitDateTimes.append(line)
-            state = 0
+authorDict = log.getAuthorEntryCount()
+for k in sorted(authorDict.keys()):
+    print k, authorDict[k]
 
-myFile.close()
-
-outputFile = open('output.txt', 'w')
-
-#print 'The date/times of the commits of ' + collabName + ' were '
-for commitDateTime in commitDateTimes:
-    outputFile.write(commitDateTime)
-outputFile.close()
 
