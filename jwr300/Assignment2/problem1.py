28812803ed7cfb1b5278ab24ea8e4412b13457c6
Warren Reed <jkr2110@gmail.com>
2013-09-23 09:48:35 -0400

    updating problem 1

--- jwr300/Assignment2/problem1.py
+++ jwr300/Assignment2/problem1.py
@@ -12,6 +12,8 @@ import dateutil.parser as dparser
 inputDate = sys.argv[1]
 commitDateTimes = []
 state = 0
+indexOfDate = 0
+DateString = ""
 tz = pytz.timezone('UTC')
 targetDate = datetime.strptime(inputDate, "%m/%d/%Y %H:%M:%S") #user specified date
 targetDate = targetDate.replace(tzinfo=tz)
@@ -20,14 +22,27 @@ targetDate = targetDate.replace(tzinfo=tz)
 myFile = open('logAfterAssignment1.txt','r')
 
 for line in myFile:
-        indexOfDate = line.find("Date:")
-        DateString = line[indexOfDate:]
-        commitDate = dparser.parse(DateString,fuzzy=True)
-        commitDate = commitDate.replace(tzinfo=tz)
-        if commitDate > targetDate:
-            commitDateTimes.append(line)
-            
+			indexOfDate = line.find("Date:")
+			if(indexOfDate != -1):
+				DateString = line[indexOfDate:]
+		        commitDate = dparser.parse(DateString,fuzzy=True)
+		        commitDate = commitDate.replace(tzinfo=tz)
+		        if commitDate > targetDate:
+		            commitDateTimes.append(line)
+	            	state = 0
+
+myFile.close()
+
+
+	            
 #Output the result
 print 'The date/times of the commits after ' + inputDate + ' were '
 for commitDateTime in commitDateTimes:
     print commitDateTime
+
+
+outputFile = open('output.txt','w')
+
+for commitDateTime in commitDateTimes:
+    outputFile.write(commitDateTime)
+outputFile.close()
