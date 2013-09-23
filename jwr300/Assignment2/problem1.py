49c8a6f03ea47ac8638905051bb8f9c0ca22354c
Warren Reed <jkr2110@gmail.com>
2013-09-23 00:50:01 -0400

    First Try of Problem 1

--- jwr300/Assignment2/problem1.py
+++ jwr300/Assignment2/problem1.py
@@ -5,14 +5,28 @@
 #Parses class github log file and compiles a list of commits after a user specified date
 
 
+import pytz
 import sys
 from datetime import datetime
-
-
+import dateutil.parser as dparser
+inputDate = sys.argv[1]
+commitDateTimes = []
+state = 0
+tz = pytz.timezone('UTC')
+targetDate = datetime.strptime(inputDate, "%m/%d/%Y %H:%M:%S") #user specified date
+targetDate = targetDate.replace(tzinfo=tz)
 
 #Read in the log data
 myFile = open('logAfterAssignment1.txt','r')
 
-
-date = datetime.strptime(sys.argv[1], "%m/%d/%Y %H:%M:%S") #user specified date
-print date
+for line in myFile:
+        #indexOfDate = line.find("Date:")
+        commitDate = dparser.parse(indexOfDate,fuzzy=True)
+        commitDate = commitDate.replace(tzinfo=tz)
+        if commitDate > targetDate:
+            commitDateTimes.append(line)
+            
+#Output the result
+print 'The date/times of the commits after ' + inputDate + ' were '
+for commitDateTime in commitDateTimes:
+    print commitDateTime
