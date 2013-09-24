d452ebd3da925cc3a304d74a72ca647637f1b5e9
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-24 10:15:27 -0400

    finalized with comments on operation and output, removed some diagnostic prints

--- rad416/Assignment2/problem2.py
+++ rad416/Assignment2/problem2.py
@@ -1,20 +1,22 @@
+# A script to take in a file of zipcodes with populations, calculate the density
+# and output those to a file, ignoring any zipcodes without a population figure
+# output is population per square mile.
+
 zipcodeInFile = open('zipCodes.csv', 'r')
-outFile = open('output_density_problem2.txt', 'w')
+
 next(zipcodeInFile) #skip header in file
 
-zipList = []
+zipList = [] #list to hold processed file contents
 
 for line in zipcodeInFile:
-  split_line = line.split(",")
+  split_line = line.split(",") #split line into list
   if split_line[10] != '\n': #test for a blank in population
     zipList.append([ split_line[0], (float(split_line[10])/float(split_line[3]))])
 
 zipcodeInFile.close()
 zipListSorted = sorted(zipList)
-print zipList
-print zipListSorted
+outFile = open('output_density_problem2.txt', 'w') #open output file
 for i in range(0,len(zipListSorted)):
-  # output = zipListSorted[i][0] + " " + str(zipListSorted[i][1])
   outFile.write(zipListSorted[i][0])
   outFile.write(" ")
   outFile.write(str(zipListSorted[i][1]))
