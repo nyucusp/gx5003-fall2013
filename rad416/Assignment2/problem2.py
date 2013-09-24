a8d770a27059abdcfcd97919e5861292dc9fde5e
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-24 01:52:16 -0400

    working on file output, sorted list is all values sorted

--- rad416/Assignment2/problem2.py
+++ rad416/Assignment2/problem2.py
@@ -1,9 +1,19 @@
 zipcodeInFile = open('zipCodes.csv', 'r')
-# outFile = open('output_density_problem2.txt', 'w')
-next(zipcodeInFile)
+outFile = open('output_density_problem2.txt', 'w')
+next(zipcodeInFile) #skip header in file
+
+zipList = []
+
 for line in zipcodeInFile:
   split_line = line.split(",")
-  if split_line[10] != '\n':
-    print "The population density of " + split_line[0] + " is " + str((float(split_line[10])/float(split_line[3])))
+  if split_line[10] != '\n': #test for a blank in population
+    zipList.append([ split_line[0], (float(split_line[10])/float(split_line[3]))])
 
 zipcodeInFile.close()
+zipListSorted = sorted(zipList)
+# print zipList
+for i in range(len(zipListSorted)):
+  output = zipListSorted[i][0] + " " + str(zipListSorted[i][1])
+  outFile.write(output)
+
+outFile.close()
