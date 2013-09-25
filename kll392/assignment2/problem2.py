ca5c6635fa2ca096679be36febb51c5286621f7c
Kara Leary <kll392@nyu.edu>
2013-09-25 14:27:26 -0400

    added comments to Problem 2

--- kll392/assignment2/problem2.py
+++ kll392/assignment2/problem2.py
@@ -1,39 +1,37 @@
+#Kara Leary
+#Urban Informatics
+#Assignment 2 - Problem 2
+
 import sys
 import csv
 
+#initialize variables for zip, population, area, density:
 currentZip = 0
 currentPop = 0
 currentArea = 0
 populationDensity = 0
 
+#set up a temporary list to hold unsorted values
 temparray = []
 
 with open('zipCodes.csv') as f:
     rows = csv.reader(f, delimiter=',')
-    rows.next()
+    rows.next()   #skip the header row
     for row in rows:
-        if (row[10] != ''):
+        if (row[10] != ''):  #I used this to eliminate rows with no population entry
             currentZip = row[1].strip()
             currentPop = float(row[10].strip())
             currentArea = float(row[7].strip())
             populationDensity = currentPop/currentArea
             
+            #create a string with the desired output values:
             thisstring = currentZip, populationDensity
-            temparray.append(thisstring)
-            
-            #print row[1].strip(), row[7].strip(), row[10].strip()
 
+            #add the current string to the unsorted list:
+            temparray.append(thisstring)  
+
+#sort the temporary list in lexicographical order
 newarray = sorted(temparray)
-
-#for entry in newarray:
-#    print entry
-
-#for line in newarray:
- #   outputFile.write(line)
-
-#print temparray
-
-#outputFile.close()
 
 outputFile = open('output_density_problem2.txt', 'w')
 for entry in newarray:
