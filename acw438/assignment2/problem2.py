b1a8892580b041f04ecb8ac05b1ef875141ac2ae
Alex Chohlas-Wood <chohlasa@gmail.com>
2013-09-25 23:27:48 -0400

    Adding comments.

--- acw438/assignment2/problem2.py
+++ acw438/assignment2/problem2.py
@@ -1,25 +1,40 @@
-import csv
+#Alex Chohlas-Wood (acw438). Assignment 2, Problem 2.
 
+'''
+Key assumption: if I find duplicate zipcode entries in zipcodes.csv,
+I will average out values for area and population for those 
+duplicates and return the average population density at the end. 
+'''
+
+import csv
+from zipcode import Zipcode
+
+#open csv with universal newline support
 zipFileRaw = open('zipcodes.csv', 'rU')
+#parse the csv into a collection of dictionaries
 zipFile = csv.DictReader(zipFileRaw, delimiter=',')
 
-zipAreas = []
+zipAreas = {}
+
 for line in zipFile:
     popl = line['Total Population per ZIP Code']
-    if popl != '':
-        popl = int(popl)
-        area = float(line['area'])
-        density = popl/area
+    if popl != '': #filter out empty population lines
+        popl = float(popl) #float because we may need to avg this value
+    area = float(line['area'])
+    if line['name'] not in zipAreas: 
+        #zipcodes that haven't been found yet
+        zipAreas[line['name']] = Zipcode(line['name'], popl, area)
     else:
-        density = "No population in this zip."
-    zipAreas.append([line['name'], density])
-
-zipAreas.sort(key=lambda variable: variable[0])
+        #average data from multiple zipcode entries
+        zipAreas[line['name']].avgFeatures(popl, area)
 
 outputFile = open("output_density_problem2.txt", 'w')
-for line in zipAreas:
-    lineContent = str(line[0]) + " " + str(line[1]) + "\n"
-    outputFile.write(lineContent)
+
+#sort the dict by keys, then output to file:
+for zips in sorted(zipAreas.iterkeys()):
+    if zipAreas[zips].population != 0:
+        outputLine = zips + " " + str(zipAreas[zips].densityCalc()) + "\n"
+        outputFile.write(outputLine)
 
 outputFile.close()
 zipFileRaw.close()
