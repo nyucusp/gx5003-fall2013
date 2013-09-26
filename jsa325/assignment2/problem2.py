3a66693e679ea0783598be3c99dc0cdde9df7c5f
thejunglejane <j.stewartadams@gmail.com>
2013-09-26 16:01:25 -0400

    Working on problem2.py

--- jsa325/assignment2/problem2.py
+++ jsa325/assignment2/problem2.py
@@ -1,1 +1,41 @@
-# given zipcodes as input, calculate population density
+# input zipcodes and output population density by zip code
+
+import sys
+
+myFile = open('zipCodes.csv','r')
+lines = []
+
+for line in myFile:
+  lines.append(line)
+myFile.close()
+
+header = lines[0].split(',') # split line 0 (first line) into header names
+header_length = len(header)
+for i in range(0, header_length):
+    header[i] = header[i].strip()
+
+indexZip = header.index('zip code') 
+indexArea = header.index('area')
+indexPopulation = header.index('population')
+
+for line in lines:
+  lineSplit = line.split(",")
+  if lineSplit[10] != '\n': # test for blank
+    lines.append([lineSplit[0], [lineSplit[7], [lineSPlit[10].rstrip()]])
+
+zipPopulationDensity = {} # create dictionary
+
+length = len(lines)
+
+for i in range(1, length):
+  if lines[i].split(',') != "\n":
+    zipPopulationDensity[lines[i].split(',')[indexZip] = (float(lines[i].split(',')[indexPopulation].strip())/float(lines[i].split(',')[indexArea].strip())]
+    
+outputFile = open('outputDensity.txt', 'w')
+
+for k in sorted(zipPopulationDensity):
+  outputFile.write("%s %s \n" % (key, zipPopdensity[key]))
+  
+outputFile.close()
+  
+  
