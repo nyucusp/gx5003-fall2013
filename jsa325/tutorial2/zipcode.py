274aef1e8fc1e6393963eedc6e6923771533656d
thejunglejane <j.stewartadams@gmail.com>
2013-09-26 17:31:29 -0400

    Update zipcode.py

--- jsa325/tutorial2/zipcode.py
+++ jsa325/tutorial2/zipcode.py
@@ -1,5 +1,7 @@
-class Zipcode:
-  number = None
-  
-  def _init_(self, number):
-    self.number = number
+from zipcode import Zipcode
+from borough import Borough
+
+boroughs['manhattan'] = Borough('manhattan')
+
+zipcode = Zipcode(value_read_from_csv)
+boroughs['manhattan'].addZipcode(zipcode)
