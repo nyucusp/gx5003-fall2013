c1928f887d512a2aa8593cf2336848c0df94d48d
Richard Allen Dunks <richarddunks@gmail.com>
2013-09-25 20:44:29 -0400

    cleaned up processing to deal with multiple entries for population and area per zipcode

--- rad416/Assignment2/problem2.py
+++ rad416/Assignment2/problem2.py
@@ -1,25 +1,42 @@
 # A script to take in a file of zipcodes with populations, calculate the density
 # and output those to a file, ignoring any zipcodes without a population figure
-# output is population per square mile.
+# output is likely population per decimal degree, though it's not clear from the 
+# source file what the units are.
+
+from _collections import defaultdict
 
 zipcodeInFile = open('zipCodes.csv', 'r')
 
 next(zipcodeInFile) #skip header in file
 
-zipList = [] #list to hold processed file contents
+zipAreaPopList = [] #list to hold processed file contents
 
-for line in zipcodeInFile:
+for line in zipcodeInFile: #iterate to populate list from file contents
   split_line = line.split(",") #split line into list
   if split_line[10] != '\n': #test for a blank in population
-    zipList.append([ split_line[0], (float(split_line[10])/float(split_line[3]))])
+    zipAreaPopList.append([ split_line[0], [ split_line[7], split_line[10].rstrip()] ] )
+
+#end loop with list of form [zipcode, [zipcodeArea, zipcodePopulation]]
 
 zipcodeInFile.close()
-zipListSorted = sorted(zipList)
+
+#convert list to dict and aggregate area and population
+zipAreaPopDict = defaultdict(list)
+for k,v in zipAreaPopList:
+    zipAreaPopDict[k].append(v)
+
+#test for multiple population and area entries in dict and aggregate
+for k in zipAreaPopDict:
+    if (len(zipAreaPopDict[k])>1): #aggregate figures for multiples
+        zipAreaPopDict[k] = [[str( float(zipAreaPopDict[k][0][0]) + float(zipAreaPopDict[k][1][0]) ),str( float(zipAreaPopDict[k][0][1]) + float(zipAreaPopDict[k][1][1]) )]]
+
+#output result
 outFile = open('output_density_problem2.txt', 'w') #open output file
-for i in range(0,len(zipListSorted)):
-  outFile.write(zipListSorted[i][0])
+for k in sorted(zipAreaPopDict):
+  popPerArea = str(float(zipAreaPopDict[k][1]) / float(zipAreaPopDict[k][0])) #variable to hold ration
+  outFile.write(k)
   outFile.write(" ")
-  outFile.write(str(zipListSorted[i][1]))
+  outFile.write(popPerArea)
   outFile.write('\n')
 
 outFile.close()
