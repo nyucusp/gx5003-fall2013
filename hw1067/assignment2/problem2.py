f8d76265e8c09fef747bf71b02c3ca87f2f62e2a
Haozhe Wang <hw1067@nyu.edu>
2013-09-28 11:58:45 -0400

    problem2

---
+++ hw1067/assignment2/problem2.py
@@ -0,0 +1,41 @@
+#problem2
+
+#haozhe Wang
+#it doesn't work!I tried my best.
+#still learning when to take the user input as a variable, when not.
+
+import csv
+
+output_data={}#empty dict for later use
+myfile=open('zipCodes.csv','r')#open file and read
+
+outputfile=open('density2.txt','w')#open a new file and write in it
+
+zip_area=csv.DictReader(myfile)#using csv module
+
+for line in zip_area:
+
+    if line['Total Population per ZIP Code']!= " ":#ignoring the blanks as suggested
+
+           population = line['Total Population per ZIP Code']
+
+           area = line['area']
+
+           density = float(population)/float(area)#trying to keep the units consistant, not sure if it will work
+
+           zipcode = line['name']
+
+           output_data[zipcode] = density
+
+           for den, zipcd in output_data:
+
+               gen_data=str(zipcd)+ " "+str(den)+ "\n" #start in a new line each time
+
+           outputfile.write(gen_data)
+
+myfile.close()
+
+outputfile.close()
+
+
+
