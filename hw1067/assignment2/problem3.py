a89a100e6eee9a5f4fafd549d20934621d2e15ad
HaozheW <hw1067@nyu.edu>
2013-09-28 23:56:02 -0400

    Create problem3.py

---
+++ hw1067/assignment2/problem3.py
@@ -0,0 +1,45 @@
+
+
+#create 3 dictionary
+
+#you have zipcode, borough and population
+
+# difine a dictionary with two features
+
+#open the given file with the required data
+
+#loop through the file
+
+
+
+#create new dict featuring zipcode and incident
+
+#open the related file
+
+#zipcode=parse(line)
+
+#check if the zipcode is already in  the dict
+
+#if the key in the dict; if not add new zipcode to the list and start counting
+#
+
+
+#new borpugh dict (borough,zipcde)
+
+
+
+#output=new dict(borough,n)
+
+#for boroughin borough dict.key( )
+
+#for zipcode in boroughdict(borough)
+
+#population += zipcodepopdict(zipcode)
+
+#incidents+=zipcodeincidents(zipcode)
+
+#n=incidents/population
+
+#outputfile=open("outputfile","w")
+
+#
