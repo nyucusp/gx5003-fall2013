373c7f2b9ac872caa0be28e4e71b9332c699eed2
AJM777 <AJM777@nyu.edu>
2013-10-02 11:05:33 -0400

    Problem 1 Submit with text file input

--- ajm777/assignment3/problem1.py
+++ ajm777/assignment3/problem1.py
@@ -2,36 +2,37 @@
 #Assignment 3
 #Problem 1: The Trip
 
-import sys
+input = open('input1.txt','r')
+data = input.readlines()
 
 #Set the starting values to loop through the input
-cycle = 1
-cycleVal = int(sys.argv[cycle])
+cycle = 0
+cycleVal = int(data[cycle])
 
 #While the value of the input integer is not zero, cycle through the input
 while cycleVal != 0:
-    n = sys.argv[cycle] #the no. of students = first value
+    n = data[cycle] #the no. of students = first value
 
     count = 1
-    index = int(cycle) + 1 
+    index = int(cycle) +1
     totalSpent = 0 
     while count <= int(n): #Cycle through the input for this trip - adding the money each student spent
-        totalSpent = float(sys.argv[index]) + totalSpent
+        totalSpent = float(data[index]) + totalSpent
         index = index + 1
         count = count + 1
 
     costPer = round(totalSpent / float(n), 2)  #Calculate the cost/student
 
     count = 1
-    index = int(cycle) + 1
+    index = int(cycle) +1
     exchange = 0
     while count <= int(n): #if a student spent < cost/student, they must exchange cost/student - their costs.
-        if float(sys.argv[index]) < costPer:  
-            exchange = exchange + (costPer - float(sys.argv[index]))
+        if float(data[index]) < costPer:  
+            exchange = exchange + (costPer - float(data[index]))
         index = index + 1
         count = count + 1
 
     print '%.2f' % exchange
     cycle = cycle + int(n) + 1
-    cycleVal = int(sys.argv[cycle])
+    cycleVal = int(data[cycle])
     
