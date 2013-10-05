d2cf9d06067cf84d77b3df56814743317661fdc4
Christopher Jacoby <cbj238@nyu.edu>
2013-10-05 08:05:13 -0400

    fixes, etc

--- cbj238/assignment3/problem2.py
+++ cbj238/assignment3/problem2.py
@@ -4,8 +4,13 @@ Urban Informatics
 Homework 3, Problem
 '''
 
+import sys
 import numpy as np
 from readInputFile import Problem2Input
+
+debug = False
+if len(sys.argv) > 1:
+	debug = True
 
 def compute_election_winner_for_case(case):
 	electionReturn = None
@@ -17,13 +22,16 @@ def compute_election_winner_for_case(case):
 	i = 0
 	while not done:
 		(done, electionReturn, percentages) = calculate_round_winner_index([x[0] for x in votes])
-		print "Vote", i+1, electionReturn, percentages
+		if debug:
+			print "Vote round:", i+1, "\n\tResults:", electionReturn, "\n\tPercent:", percentages*100
 
 		if not done:
 			eliminate_tied_candidates(candidates, votes, percentages)
 
 		i += 1
-		if i > 5:
+		# There are never more than 20 candidates, so we should never have more than 20 individual votes.
+		# This is here just to avoid infinite loops, basically.
+		if i > 20:
 			break
 
 	return np.array(candidates)[electionReturn]
@@ -65,23 +73,39 @@ def eliminate_tied_candidates(candidates, runoffMatrix, percentages):
 	If there are candidates tied for last, remove them from the options
 	'''
 	# Find the candadites with the smallest percentage. (1 or more)
-	removeIndecies = (percentages == percentages.min()).nonzero()
+	# ** I am assuming that I am throwing out people with zero votes anyway, since
+	# once I remove them, they get zero votes, and otherwise it would break my approach.
+	removeIndecies = (percentages == percentages[percentages.nonzero()].min()).nonzero()[0]
+	if debug:
+		print "Removing: ", removeIndecies
 
-	for vote in runoffMatrix:
-		for candInd in removeIndecies:
-			# This is +1 because candInd is 0-based and the voting system is 1-based
-			vote.remove(candInd + 1)
+	try:
+		for vote in runoffMatrix:
+			for candInd in removeIndecies:
+				# This is +1 because candInd is 0-based and the voting system is 1-based
+				vote.remove(candInd + 1)
+	except ValueError:
+		print "Vote (", vote, ") is missing", candInd+1
 
-	print "eliminating: ", np.array(candidates)[removeIndecies],
-	print "New matrix: ", runoffMatrix
+	if debug:
+		print "\teliminating: ", np.array(candidates)[removeIndecies]
+		print "New matrix: \n", np.array(runoffMatrix)
 
 
 voteExamples = Problem2Input()
-print "Data:", len(voteExamples.data), voteExamples.data
+if debug:
+	print "There are ", len(voteExamples.data), "cases to follow:"
+
+	# print voteExamples.data
 
 for case in voteExamples.data:
+	if debug:
+		print "====== New Election ======="
+		print "Candidates: ", case[0]
 	winner = compute_election_winner_for_case(case)
 
+	if debug:
+		print "Election Winner(s):",
 	print winner
 	print         # Adds a blank line after the results
 
