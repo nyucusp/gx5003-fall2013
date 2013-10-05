a5ba13d400d4c7d5d6106d3117f9609852a9db09
Richard Allen Dunks <richarddunks@gmail.com>
2013-10-04 20:43:04 -0400

    intern and bind grid to 2-d list with all characters in downcase

---
+++ rad416/Assignment3/problem4.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+inputFile = open('input4.txt','r')
+
+inputQue = deque()
+
+for lines in inputFile:
+  inputQue.append(lines.rstrip())
+
+instances = int(inputQue.popleft())
+
+#start while loop for instances
+
+inputQue.popleft() #kill blank line
+
+rows, columns = inputQue.popleft().split(" ")
+
+grid = [] # grid list
+for i in range(0,int(rows)):
+    grid.append(list(inputQue.popleft().lower()))
+
+
+
+#end while loop for instances
