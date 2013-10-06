00ccceacd0592e4a5c51c95057c6f158a769fe5e
Warren Reed <jkr2110@gmail.com>
2013-10-06 01:04:33 -0400

    working version of problem 2

--- jwr300/Assignment3/problem2.py
+++ jwr300/Assignment3/problem2.py
@@ -3,8 +3,7 @@
 #Principles of Urban Informatics
 #Assignment 3, Problem 2
 
-
-from collections import defaultdict
+import sys
 
 myFile = open('input2.txt', 'r')
 numElections = int(myFile.readline())
@@ -13,65 +12,65 @@ currentElection = 0
 
 myFile.readline() #read blank line
 
-def ballotCounter(candidates, ballots):
-    isWinner = False
+def ballotCounter(candidates, ballots_list):
     #first check to see if first candidate has greater than 50% of the votes 
     candidatesdict = candidates.copy()
+    isWinner = False
+    ballotLength = len(ballots_list)
 
     for key in candidatesdict:
-        voteCount = 0
-        for j in range(0,len(ballots)):
-            if (candidatesdict[key] == int(ballots[j].split()[0])):
-                voteCount += 1
+        voteCount = 0   ####fix bug on vote count
+        for j in range(0,ballotLength):
+            if (candidatesdict[key] == int(ballots_list[j].split()[0])):
+            	voteCount += 1
+                
         candidatesdict[key] = voteCount
-    print candidatesdict
     
     #check if one candidate has more than 50% of the votes 
     for key in candidatesdict:
-        if float(candidatesdict[key])/len(ballots) > 0.5:
+        if float(candidatesdict[key])/ballotLength > .5:
             isWinner = True
             print key
             
     #all candidates are tied 
     tally = [(votes, name) for name, votes in candidatesdict.items()]
-    print "tally below:"
-    print tally
     
     
-    if max(tally) == min(tally) and len(tally) > 1:
+    if max(tally)[0] == min(tally)[0] and len(tally) > 1:
         isWinner = True
+        
         for key in candidatesdict:
             print key
     
-    candidates_runoff = candidatesdict.copy()
-    new_ballots = ballots[:]
+    candidates_runoff = candidates.copy()
+    new_ballots = ballots_list[:]
     lowest_candidates = {}
     if (isWinner == False):
         for key in candidatesdict:
-            if candidatesdict[key] == min(tally):
-                lowest_candidates[key] = candidatesdict[key]
+            if candidatesdict[key] == min(tally)[0]:
+                lowest_candidates[key] = candidates[key]
                 del candidates_runoff[key]
-    
-        for i in range(0,len(new_ballots)):
+
+        for k in range(0,len(new_ballots)):
             for key in lowest_candidates:
-                new_ballots[i] = new_ballots[i].translate(None, str(lowest_candidates[key])).lstrip()
-    
+                new_ballots[k] = new_ballots[k].translate(None, str(lowest_candidates[key])).lstrip()
+
         ballotCounter(candidates_runoff, new_ballots)
                 
     
 while (currentElection < numElections):
     numCandidates = int(myFile.readline())
-    print "numCandidates: %s" % (numCandidates)
+
     candidates = {}
     for i in range(0,numCandidates):
         candidates[myFile.readline().strip()] = int(i+1)
-        print candidates
+
     ballots = []
     line = myFile.readline()
-    while(line != "" and line != None and line != "/n"):
+    while(line != "" and line != "/n"):
         ballots.append(line.strip())
-        print ballots
+
         line = myFile.readline()
     ballotCounter(candidates, ballots)
-    print len(ballots)
+
     currentElection += 1
