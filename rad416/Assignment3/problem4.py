a08d1a4f7318c8ce0e27312e0c26915fc7fe37de
Richard Allen Dunks <richarddunks@gmail.com>
2013-10-06 23:48:38 -0400

    added gridCheck method for searching words. Still need to work on reverse diag

--- rad416/Assignment3/problem4.py
+++ rad416/Assignment3/problem4.py
@@ -26,6 +26,49 @@ wordsList = []
 for i in range(0,words):
   wordsList.append(inputQue.popleft())
 
+def gridCheck():
+  print "Rows\n"
+  for i in range(int(rows)):
+    stringCat = ""
+    for j in range(int(columns)):
+      stringCat += grid[i][j]
+    wordCheck(stringCat)
 
+  print "\nColumns\n"
+  for j in range(int(columns)):
+    stringCat = ""
+    for i in range(int(rows)):
+      stringCat += grid[i][j]
+    wordCheck(stringCat)
+
+  print "\nDiagonals\n"
+  for k in range(0,max(int(rows),int(columns))):
+    stringCat = ""
+    j = (min(k,int(columns)))
+    for i in range(0,min(k+1,int(rows))):
+      stringCat += grid[i][j]
+      j += -1
+    wordCheck(stringCat)
+
+  print "\nResidual\n"
+
+  for k in range(1,int(rows)):
+    stringCat = ""
+    j = int(columns) - 1
+    for i in range(k,int(rows)):
+      stringCat += grid[i][j]
+      j += -1
+    wordCheck(stringCat)
+
+#end method gridCheck
+
+def wordCheck(stringCat):
+    for i in range(len(wordsList)):
+        if stringCat.find(wordsList[i]) >= 0:
+            print "Found " + wordsList[i] + " in " + stringCat
+        elif stringCat[::-1].find(wordsList[i]) >= 0: 
+            print "Found " + wordsList[i] + " in " + stringCat
+#        else: 
+#            print "Didn't find " + wordsList[i] + " in " + stringCat
 
 #end while loop for instances
