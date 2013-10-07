fac14eb56d252d63877e69d79164d17a06fe09a3
Richard Allen Dunks <richarddunks@gmail.com>
2013-10-07 03:47:21 -0400

    operational program

--- rad416/Assignment3/problem4.py
+++ rad416/Assignment3/problem4.py
@@ -1,13 +1,116 @@
 from collections import deque
 
-inputFile = open('input4.txt','r')
+def gridCheck():
+#   Rows
+    for i in range(int(rows)):
+        stringCat = ""
+        for j in range(int(columns)):
+            stringCat += grid[i][j]
+        wordCheck(stringCat,i,j,"row")
+        
+#   Columns
+    for j in range(int(columns)):
+        stringCat = ""
+        for i in range(int(rows)):
+            stringCat += grid[i][j]
+        wordCheck(stringCat,i,j,"column")
 
-inputQue = deque()
+#   LR Diagonals
+    for k in range(0,max(int(rows),int(columns))):
+        stringCat = ""
+        j = (min(k,int(columns)))
+        for i in range(0,min(k+1,int(rows))):
+            stringCat += grid[i][j]
+            j += -1
+        wordCheck(stringCat,i,j,"lrDiag")
 
-for lines in inputFile:
+#   LR Residual
+    for k in range(1,int(rows)):
+        stringCat = ""
+        j = int(columns) - 1
+        for i in range(k,int(rows)):
+            stringCat += grid[i][j]
+            j += -1
+        wordCheck(stringCat,i,j,"lrDiag")
+
+#   RL Diagonals
+    l = int(columns) - 1
+    for k in range(1,int(columns)):
+        stringCat = ""
+        j = l
+        for i in range(0,min(k,int(rows))):
+            stringCat += grid[i][j]
+            j += 1
+        wordCheck(stringCat,i,j-1,"rlDiag")
+        l += -1
+
+#   RL Residuals
+    l = int(rows)
+    for k in range(0,int(rows)):
+        stringCat = ""
+        j = 0
+        while (j < l):
+            for i in range(k,int(rows)):
+                stringCat += grid[i][j]
+                j += 1
+            wordCheck(stringCat,i,j-1,"rlDiag")
+            l +=-1
+#end method gridCheck
+
+def wordCheck(stringCat,row,col,stringType):
+    for i in range(len(wordsList)):
+        stringCatRev = stringCat[::-1]
+        if (wordsList[i][1] == -1 and stringCat.find(wordsList[i][0]) >= 0):
+            # print "Word " + wordsList[i][0] + " in the string " + stringCat + " of type " + stringType + " that ends at " + str(row) + "," + str(col) + " at index " + str(stringCat.find(wordsList[i][0]))
+            if (stringType == "row"):
+                col = col - len(stringCat) + 1 + stringCat.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+            elif (stringType == "column"):
+                row = row - len(stringCat) + 1 + stringCat.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+            elif (stringType == "rlDiag"):
+                row = row - len(stringCat) + 1 + stringCat.find(wordsList[i][0])
+                col = col - len(stringCat) + 1 + stringCat.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+            elif (stringType == "lrDiag"):
+                row = row - len(stringCat) + 1 + stringCat.find(wordsList[i][0])
+                col = col + len(stringCat) - 1 - stringCat.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+        elif (wordsList[i][1] == -1 and stringCatRev.find(wordsList[i][0]) >= 0): 
+            # print "Word " + wordsList[i][0] + " in the string " + stringCatRev + " of type " + stringType + " that ends at " + str(row) + "," + str(col) + " at index " + str(stringCatRev.find(wordsList[i][0]))
+            if (stringType == "row"):
+                col = col - stringCatRev.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+            elif (stringType == "column"):
+                row = row - stringCatRev.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+            elif (stringType == "rlDiag"):
+                row = row - stringCatRev.find(wordsList[i][0])
+                col = col - stringCatRev.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+            elif (stringType == "lrDiag"):
+                row = row - stringCatRev.find(wordsList[i][0])
+                col = col + stringCatRev.find(wordsList[i][0])
+                #print "The start of the word is " + str(row) + "," + str(col)
+                wordsList[i][1] = [row,col]
+
+
+#begin script operation
+inputFile = open('input4.txt','r') #read in input file
+
+inputQue = deque() #deque for holding file contents
+
+for lines in inputFile: #read input into deque
   inputQue.append(lines.rstrip())
 
-instances = int(inputQue.popleft())
+instances = int(inputQue.popleft()) #instances of the crossword search
 
 #start while loop for instances
 
@@ -26,78 +129,11 @@ wordsList = []
 for i in range(0,words):
   wordsList.append([inputQue.popleft().lower(),-1])
 
-def gridCheck():
-#    print "Rows\n"
-    for i in range(int(rows)):
-        stringCat = ""
-        for j in range(int(columns)):
-            stringCat += grid[i][j]
-        wordCheck(stringCat,i,j,"row")
-        
-#    print "\nColumns\n"
-    for j in range(int(columns)):
-        stringCat = ""
-        for i in range(int(rows)):
-            stringCat += grid[i][j]
-        wordCheck(stringCat,i,j,"column")
+gridCheck()
 
-#    print "\nLR Diagonals\n"
-    for k in range(0,max(int(rows),int(columns))):
-        stringCat = ""
-        j = (min(k,int(columns)))
-        for i in range(0,min(k+1,int(rows))):
-            stringCat += grid[i][j]
-            j += -1
-        wordCheck(stringCat,i,j,"lrDiag")
-
-#        print stringCat
-
-#    print "\nLR Residual\n"
-
-    for k in range(1,int(rows)):
-        stringCat = ""
-        j = int(columns) - 1
-        for i in range(k,int(rows)):
-            stringCat += grid[i][j]
-            j += -1
-        wordCheck(stringCat,i,j,"lrDiag")
-
-#        print stringCat
-
-#    print "\nRL Diagonals\n"
-    l = int(columns) - 1
-    for k in range(1,int(columns)):
-        stringCat = ""
-        j = l
-        for i in range(0,min(k,int(rows))):
-            stringCat += grid[i][j]
-            j += 1
-        wordCheck(stringCat,i,j-1,"rlDiag")
-#        print stringCat
-        l += -1
-
- #   print"\nRL Residuals\n"
-    l = int(rows)
-    for k in range(0,int(rows)):
-        stringCat = ""
-        j = 0
-        while (j < l):
-            for i in range(k,int(rows)):
-                stringCat += grid[i][j]
-                j += 1
-            wordCheck(stringCat,i,j-1,"rlDiag")
- #           print stringCat
-            l +=-1
-#end method gridCheck
-
-def wordCheck(stringCat,row,col,stringType):
-  for i in range(len(wordsList)):
-    stringCatRev = stringCat[::-1]
-    if (wordsList[i][1] == -1 and stringCat.find(wordsList[i][0]) >= 0):
-      print "Word " + wordsList[i][0] + " in the string " + stringCat + " of type " + stringType + " that ends at " + str(row) + "," + str(col) + " at index " + str(stringCat.find(wordsList[i][0]))
-    elif (wordsList[i][1] == -1 and stringCatRev.find(wordsList[i][0]) >= 0): 
-      print "Word " + wordsList[i][0] + " in the string " + stringCatRev + " of type " + stringType + " that ends at " + str(row) + "," + str(col) + " at index " + str(stringCatRev.find(wordsList[i][0]))
-#        else: 
-#            print "Didn't find " + wordsList[i] + " in " + stringCat
-
+#output result
+for element in wordsList:
+    for i in element[1]:
+        print i+1,
+    print ""
 #end while loop for instances
