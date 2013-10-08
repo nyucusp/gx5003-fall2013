8c7665a39bb4782e65068ac1e85ba6605e3031d0
L Chaitanya V <lcv232@nyu.edu>
2013-10-07 23:11:13 -0400

    Message2

--- lcv232/Assignment3/problem1.py
+++ lcv232/Assignment3/problem1.py
@@ -1,27 +1,37 @@
 import sys
-d={}
-while(True):
-    a=input("")   
-    if (int(a) <= 1000):
-        if (int(a) != 0):
-            L=[]
-            for i in range(a):
-                b=input("")
-                if (float(b) <= 10000.00):
-                    if isinstance(b, float):
-                        L.append(b)
-                
-                    else:
-                        if (int(b) == 0):
-                            break
-                        else:
-                            break
-            d[a]=L
-        elif (int(a) == 0):
-            break
-print "OUTPUT"
-for data in d.keys():
-    a=0
-    for dt in d[data]:
-        a=int(a)+int(dt)
-    print  float(a/data)
+import math
+
+input = open('input1.txt','r')
+value1 = input.readlines()
+
+
+loops = 0    # Setting initial value = 0 for looping. 
+trip1 = int(value1[loops])
+
+while trip1 != 0:  # Conditioning the reason when data is non-zero.
+    x = value1[loops] #Letting the case start with first value
+
+
+    y = 1
+    counter = int(loops) +1
+    Expenditure = 0 
+    while y <= int(x):
+        Expenditure = float(value1[counter]) + Expenditure
+        counter = counter + 1
+        y = y + 1
+
+    indi_cost = round(Expenditure / float(x), 2)  # Finding the value cost per student and rounding it; assigning to variable.
+
+    y = 1
+    counter = int(loops) +1
+    cost_exch = 0
+    while y <= int(x):	 
+        if float(value1[counter]) < indi_cost:  
+            cost_exch = cost_exch + (indi_cost - float(value1[counter]))  # Exchanging costs if money spent less subtracting costs after exchange.
+        counter = counter + 1
+        y = y + 1
+
+
+    print '%.2f' % cost_exch
+    loops = loops + int(x) + 1
+    trip1 = int(value1[loops])
