46443b58baeb10ee6e25354fa59c6d91c931033a
thejunglejane <j.stewartadams@gmail.com>
2013-10-07 20:40:56 -0400

    Update problem3.py

--- jsa325/assignment3/problem3.py
+++ jsa325/assignment3/problem3.py
@@ -15,43 +15,6 @@ Parse the input for integer pairs and splice input into a new list of scenarios
 the authors and their Erdos numbers. Return a dictionary searchDict with keys = 
 authors and values = Erdos numbers.
 """
-
-# def scenarioSolve(current_scenario):
-# num_papers = int(current_scenario[0].split()[0])
-#  papers_list = current_scenario[1:num_papers+1]
-#  authors_to_search = current_scenario[num_papers+1:]
-
-    
-#  authors_dictionary = find_all_authors(papers_list)
-#  papers_dictionary = find_all_papers(authors_dictionary, papers_list)
-    
-#  A = create_adjacency_matrix(papers_dictionary)
-    
-#  erdos_key = -1
-#  for key,value in authors_dictionary.items():
-#    if value == 'Erdos, P.':
-#      erdos_key = key
-#  authors_erdos_numbers = {}
-    
-#  for i in range(1, len(authors_dictionary)+1):
-#    count = 0
-#      for k in range(1, num_papers + 1):
-#        if numpy.linalg.matrix_power(A, k)[i-1, erdos_key - 1] != 0 and count == 0 and erdos_key != -1:
-#          count += 1
-#          authors_erdos_numbers[authors_dictionary[i]] = k
-#        elif k == num_papers and count == 0:
-#          authors_erdos_numbers[authors_dictionary[i]] = "infinity"
-    
-#  authors_erdos_numbers['Erdos, P.'] = 0
-    
-#  for author in authors_to_search:
-#    mark = 0
-#    for key in authors_erdos_numbers:
-#      if author[:-1] == key or author[:-1]+"." == key:
-#        print key + " " + str(authors_erdos_numbers[key])
-#        mark += 1
-#    if mark == 0:
-#      print author[:-1] + " " + "infinity"
             
 """
 1. Define a function listScenarios that splices each scenario into two lists: a list of papers 
