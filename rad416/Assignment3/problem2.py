40ad12873e2d94223a4a0722177014fd070701bd
Richard Allen Dunks <richarddunks@gmail.com>
2013-10-07 22:25:23 -0400

    basic functionality implemented

--- rad416/Assignment3/problem2.py
+++ rad416/Assignment3/problem2.py
@@ -1,6 +1,32 @@
 from _collections import defaultdict
 from collections import deque
 
+def ballotCounter(ballotList):
+    ballotDict = defaultdict(int)
+    for i in ballotList:
+        ballotDict[i[0]] += 1
+    return ballotDict
+
+# check for majority winner
+def calcWinner(ballotList): 
+    ballotSum = 0
+    for i in range(len(ballotList)):
+        ballotSum += ballotDict[k]
+
+    for k in ballotDict:
+        if (float(ballotDict[k]) / float(ballotSum) > 0.5):
+            return k
+        else:
+            return -1
+
+def genLoserList(ballotDict):
+    minVotes = min(ballotDict.values())
+    loserList = []
+    for k,v in ballotDict.iteritems():
+    if v == minVotes:
+        loserList.append(k)
+    return loserList
+
 inputFile = open('input2.txt','r')
 
 inputQue = deque()
@@ -30,29 +56,39 @@ ballotList = [] #list of deques containing ballots
 for i in range(len(inputQue)):
     ballotList.append( deque(inputQue[i].split(" ")) )
 
-numVoters = len(ballotList)
-
 # generate dict key on candidate number and value number votes
 ballotDict = ballotCounter(ballotList)
 
 #check for majority winner in first round
-winnerCheck = calcWinner(ballotList) 
+winnerCheck = calcWinner(ballotDict) 
 
 while (winnerCheck == -1): #loop until find majority winner
     #loser check
-    minVotes = min(ballotList.values())
-    loserList = []
+    
+    loserList = genLoserList()
+    for i in range(len(ballotList)):
+        for loser in loserList:
+            print filter(lambda x : x != loser, ballotList[i])
 
-    condition = True
-    while condition:
-        loserKey.append(min(ballotDict, key=ballotDict.get)) #find min
+    #filter loser(s) from ballotList
+
+    #recalc ballotDict
+    ballotDict = ballotCounter(ballotList)
+
+
+
+
+
+    # condition = True
+    # while condition:
+    #     loserKey.append(min(ballotDict, key=ballotDict.get)) #find min
         
 
-        #test for other min in dict, if not, break list
-        if ( returnValCount(ballotList,minVotes) > 1 ):
-            #pop item from dict
-        else:  
-            condition = False #break while loop
+    #     #test for other min in dict, if not, break list
+    #     if ( returnValCount(ballotList,minVotes) > 1 ):
+    #         #pop item from dict
+    #     else:  
+    #         condition = False #break while loop
         
     #filter loser
 
@@ -61,40 +97,7 @@ while (winnerCheck == -1): #loop until find majority winner
   
 
 
-print candidateDict[winnerCheck]
+print candidateDict[int(winnerCheck)]
   # loopCounter += 1 #incrememt the control counter
 
 # transform ballotList into dictionary keyed on candidate number
-def ballotCounter(ballotList):
-    ballotDict = defaultdict(int)
-    for i in ballotList:
-        ballotDict[i[0]] += 1
-    return ballotDict
-
-# check for majority winner
-def calcWinner(ballotDict): 
-    ballotSum = 0
-    for k in ballotDict:
-        ballotSum += ballotDict[k]
-
-    for k in ballotDict:
-        if (ballotDict[k] / ballotSum > 0.5):
-            return k
-        else:
-            return -1
-
-#check for loser or loser tie
-def calcLoser(ballotDict):
-    ballotSum = 0
-    for k in ballotDict:
-        ballotSum += ballotDict[k]
-
-    for k in ballotDict:
-
-def returnValCount(dictionary, value):
-    return ballotDict.values().count(value)
-
-def loserList(ballotDict):
-  #find min
-  #find count of min
-  #iterate through dict and return key for losers in list
