4e57fcf74b14f70305924329485a2972ab2762ec
justinjee <justinjee@gmail.com>
2013-10-10 13:34:20 -0400

    added problem1. not sure what to do if change is <

---
+++ jj1006/assignment3/problem1.py
@@ -0,0 +1,30 @@
+import math
+
+def change(costs):
+	#COSTS MUST BE IN CENTS
+	m = 0.0;
+	for c in costs:
+		m=m+c
+	m=m/len(costs)
+	val=0.0
+	for c in costs:
+		if c>=m:
+			val=val+c-math.ceil(m)
+		else:
+			val=val+math.floor(m)-c
+	return int(val/2)
+
+
+finput = open('input1.txt','r')
+n=-1
+costs = []
+while not n==0:
+	line=finput.readline().rstrip()
+	if not '.' in line:
+		if n>0:
+			cstr = str(change(costs))
+			print "$"+cstr[0:(len(cstr)-2)]+"."+cstr[(len(cstr)-2):len(cstr)]
+			costs = []
+		n = int(line)
+	else:
+		costs.append(100*float(line))
