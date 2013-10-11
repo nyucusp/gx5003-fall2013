c71f0011b7b45f816018901bc669ef6f98ebb3a7
lz1023 <lz1023@nyu.edu>
2013-10-10 23:36:37 -0400

    update

--- lz1023/assignment3/problem3.py
+++ lz1023/assignment3/problem3.py
@@ -6,12 +6,25 @@ myFile.close
 
 
 
-database_num=int(readdata[1].split()[0])
-name_num=int(readdata[1].split()[1])
-database=[]
-for i in ()
+author_database=[]
+author=[]
+def get_casedata(readdata):
+	for i in range(0,int(readdata[0])):
+		startline=0
+		database_num=int(readdata[startline+1].split()[0])#get the number of lines of database
+		name_num=int(readdata[startline+1].split()[1])#get the number of lines of names
 
-
-
-
+		database=readdata[startline+2:startline+2+database_num]#get the data of database
+		name=readdata[startline+2+database_num:startline+database_num+name_num+2]#get the data of author names
+		startline=startline+database_num+name_num+2#the startline of next case
+		
+		for j in database:
+			allnames=j.split(",")[0]#delete the paper info
+			allname=allnames[:-1]#delete the :
+			namedata=allname.split(".,")#split the names
+			namedata=[everyname+'.' for everyname in namedata]#add "." into the end of each name
+			author_database.append(namedata)
+		author.append((author_database,name_num))
+		print author	
+author=get_casedata(readdata)
 
