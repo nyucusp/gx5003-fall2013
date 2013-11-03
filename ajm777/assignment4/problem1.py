2013aeef827249f600674c842b22469600af6469
AJM777 <AJM777@nyu.edu>
2013-11-03 18:29:35 -0500

    Updated Table Values, Final Commit Problem 1

--- ajm777/assignment4/problem1.py
+++ ajm777/assignment4/problem1.py
@@ -16,17 +16,17 @@ db = MySQLdb.connect(host="localhost",user="ajm777",passwd="pepper89",db="course
 cur = db.cursor()  
 
 #_______________CREATING DATABASES_________________
-create_zip = "create table zip_code (zip_code varchar(10), area float not null, pop_by_zip int)"
-create_bor = "create table bor (zip_code varchar(10) not null, name text not null)"
-create_inc = "create table inc (address varchar(500), zip_code varchar(10) not null, num_inc int)"
+create_zip = "create table zips (zip_code int not null, area float not null, pop_by_zip int)"
+create_bor = "create table bor (zip_code int not null, name text not null)"
+create_inc = "create table inc (address varchar(500), zip_code int not null, num_inc int)"
 
 #Stop the warnings from appearing from the drop if exists commmand
-import warnings
-warnings.filterwarnings("ignore", "Unknown table.*")
-#So I can create the table over again when needed, use the drop if exists command
-cur.execute('DROP TABLE IF EXISTS create_zip')
-cur.execute('DROP TABLE IF EXISTS create_bor')
-cur.execute('DROP TABLE IF EXISTS create_inc')
+#import warnings
+#warnings.filterwarnings("ignore", "Unknown table.*")
+#So I can create the table over again, use the drop if exists command (for the purpose of testing)
+cur.execute(create_zip)
+cur.execute(create_bor)
+cur.execute(create_inc)
 
 #____________________FILLING DATABASES_________________
 #Getting the zip code data in a format to be put into the table
@@ -37,9 +37,9 @@ for line in zip_data_read:
     pop_by_zip = str(line['Total Population per ZIP Code'])
     zip_val_list =  list(zip_val)
     if zip_val_list[3] != 'H' and pop_by_zip != ' ' and pop_by_zip != '': #ignores dirty data with HH zipcode or emptly pop value
-        insert_command = "insert into zip_code values(" + "'" + zip_val + "'" + "," + "'" + area + "'" + "," + "'" + pop_by_zip + "'"  ");"
+        insert_command = "insert into zips values(" + "'" + zip_val + "'" + "," + "'" + area + "'" + "," + "'" + pop_by_zip + "'"  ");"
         cur.execute(insert_command)
-#print "Zip Table Filled!"
+print "Zip Table Filled!"
 
 #Getting borough data in format to be put into the table & Filling the bor table
 bor_data_read = csv.reader(bor_data_file)
@@ -48,7 +48,7 @@ for line in bor_data_read:
     bor_name = str(line[1])
     insert_command = "insert into bor values(" + "'" + zip_val + "'" + "," + "'" + bor_name + "'" ");"
     cur.execute(insert_command)
-#print "Borough Table Filled!"
+print "Borough Table Filled!"
 
 #Getting incident data in format for inupt & filling the inc table
 inc_data_read = csv.DictReader(inc_data_file)
@@ -64,7 +64,7 @@ for line in inc_data_read:
     if zip_val != '':
         insert_command = "insert into inc values(" + "'" + str(address) + "'" + "," + "'" + zip_val + "'" + "," + "'" + inc_count + "'"  ");"
         cur.execute(insert_command)
-#print "Incident Table Filled!"
+print "Incident Table Filled!"
 
 db.commit()	
 db.close()
