c512397e21ee84f4400bc47ba6e326d2c31134c0
Richard Allen Dunks <richarddunks@gmail.com>
2013-11-03 01:31:34 -0400

    organized output of problem4.py

--- rad416/Assignment4/problem4.py
+++ rad416/Assignment4/problem4.py
@@ -12,7 +12,7 @@ with db:
   # The Cursor object will let you execute the sql commands
   cur = db.cursor()
 
-  query = "SELECT address FROM incidents i INNER JOIN boroughs b ON b.zipcode = i.zipcode WHERE borough = 'Manhattan' AND address > ''"
+  query = "SELECT address FROM incidents i INNER JOIN boroughs b ON b.zipcode = i.zipcode WHERE borough = 'Manhattan' AND address > '' GROUP BY address ORDER BY address"
   cur.execute(query)
   result = cur.fetchall()
   for row in result:
