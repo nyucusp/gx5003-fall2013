5c57986635798aeb581a55745d6b6ce859d34874
Christopher Jacoby <cbj238@nyu.edu>
2013-11-04 17:58:47 -0500

    problems 4,5,6

---
+++ cbj238/assignment4/problem6.py
@@ -0,0 +1,25 @@
+"""
+	assignment 4 problem6.py
+	author: Christopher Jacoby
+	Principles of Urban Informatics, Fall 2013
+
+	 List the set of zip codes in Manhattan along with their population where incidents have occurred.
+"""
+
+from dbmgr import dbMgr
+import sys
+
+def main(args):
+	db = dbMgr()
+	print "Computing query (this may take a moment...)"
+	query = "SELECT B.zipcode, Z.population from boroughs B, zipcodes Z, incidents I where B.borough = 'Manhattan' && B.zipcode = I.zip && B.zipcode = Z.name;"
+	db.run_sql(query)
+
+	results = db.results()
+	for item in results:
+		print item[0], item[1]
+
+	db.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
