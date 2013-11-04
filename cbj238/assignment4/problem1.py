72dd0acf26e11b8a1b6aeccbac32968abc0f2fd2
Christopher Jacoby <cbj238@nyu.edu>
2013-11-04 13:00:27 -0500

    moved db access to separate file; started problem2

--- cbj238/assignment4/problem1.py
+++ cbj238/assignment4/problem1.py
@@ -1,75 +1,24 @@
-import MySQLdb
+"""
+    assignment 4 problem1.py
+    author: Christopher Jacoby
+    Principles of Urban Informatics, Fall 2013
+"""
+
+from dbmgr import dbMgr
 from parsecsv import *
 
 BOROUGHS_TABLE="boroughs"
 ZIP_TABLE="zipcodes"
 INCIDENTS_TABLE="incidents"
 
-class dbMgr:
-    def __init__(self):
-        try: 
-            #connect to database
-            self.db = MySQLdb.connect(host="localhost",
-                                 user="cbj238",
-                                 passwd="1122334455",
-                                 db="coursedb")
-
-            self.cur = self.db.cursor() 
-        except MySQLdb.Error:
-            print "There was a problem in connecting to the database. Please ensure tha tthe database exists on the local host system."
-            exit(-1)
-
-    def run_sql(self, command):
-        if command is not None and len(command) > 0:
-            # The Cursor object lets you execute the sql commands
-
-            try: 
-                self.cur.execute(command)
-
-            # self.cur.close()
-             # OperationalError
-            except MySQLdb.OperationalError, e :
-                print "Some of the information you have passed is not valid. Please check it before trying to use this program again. You may also use '-h' to see the options available."
-                print "The exact error information reads as follows:%s" %(e)
-                raise
-            # DataError
-            # ProgrammingError
-            except (MySQLdb.DataError, MySQLdb.ProgrammingError), e:
-                print "An irrecoverable error has occured in the way your data was to be processed. This application must now close. An error message describing the fault has been sent to the development team. Apologies for any inconvenience."
-                raise
-            # IntegrityError
-            except MySQLdb.IntegrityError, e:
-                print "An irrecoverable database error has occurred and this process must now end. An error message describing the fault has been sent to the database administrator. Apologies for any inconvenience."
-                raise
-            # InternalError
-            # NotSupportedError
-            except (MySQLdb.InternalError, MySQLdb.NotSupportedError), e:
-                print "An irrecoverable error has occurred and this process must now end. An error message describing the fault has been sent to the appropriate staff. Apologies for any inconvenience."
-                raise
-            except MySQLdb.Warning:
-                pass
-
-    def commit(self):
-        # Must commit changes before closing connection Else data won't be inserted.    
-        self.db.commit()
-
-    def close(self):
-
-        # Close the connection
-        self.db.close()
-
 def clean_db(db):
-    try:
-        clean_command = "DROP TABLE IF EXISTS zipcodes"
-        db.run_sql(clean_command)
-        clean_command = "DROP TABLE IF EXISTS incidents"
-        db.run_sql(clean_command)
-        clean_command = "DROP TABLE IF EXISTS boroughs"
-        db.run_sql(clean_command)
-        db.commit()
-    except MySQLdb.Warning:
-        # Just ignore those warnings about them not existing.
-        pass
+    clean_command = "DROP TABLE IF EXISTS zipcodes"
+    db.run_sql(clean_command)
+    clean_command = "DROP TABLE IF EXISTS incidents"
+    db.run_sql(clean_command)
+    clean_command = "DROP TABLE IF EXISTS boroughs"
+    db.run_sql(clean_command)
+    db.commit()
 
 def get_sql_column(name, vartype, primary_key):
     retStr = "{0} ".format(name)
@@ -88,8 +37,7 @@ def get_sql_column(name, vartype, primary_key):
 
 def create_schema_from_header(tableName, data, primary_key):
     # print "Schema:", tableName, data[0], data[1]
-    # All we need are the names and types for each field.
-
+    # All we need are the names and types for each field
     primary_key_str = primary_key
     createCommand = "create table " + tableName + " ("
     if primary_key is None:
---
+++ cbj238/assignment4/problem2.py
@@ -0,0 +1,43 @@
+"""
+	assignment 4 problem2.py
+	author: Christopher Jacoby
+	Principles of Urban Informatics, Fall 2013
+
+	Given a zip code as input in the command line, 
+	compute the population density of that zip code. 
+
+	You should NOT use a "select *" query and parse through the result.
+"""
+
+from dbmgr import dbMgr
+import sys
+
+def validate_input(zipInput):
+	"""
+	Returns true if zipInput is a valid numerical zip code. Else, false
+	"""
+	return zipInput.isdigit()
+
+def main(args):
+	if len(args) > 0:
+		db = dbMgr()
+		zipInput = args[0]
+		if validate_input(zipInput):
+			pass
+
+		query = "select area, population from zipcodes where name={0}".format(zipInput)
+		db.run_sql(query)
+
+		results = db.results()
+		if len(results) > 0:
+			(area, population) = results[0]
+			print (population / area)
+		else:
+			print "No entry for this zip."
+
+		db.close()
+	else:
+		print "Error: Invalid input. Please enter a zip code."
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
