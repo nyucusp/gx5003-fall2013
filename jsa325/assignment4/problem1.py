8f7ce42915d1787fbfd5f7c5acb909e160f83b70
JSAdams <jsa325@nyu.edu>
2013-11-04 17:25:33 -0500

    Insert data from zipcodes.csv into zipcodes table

--- jsa325/assignment4/problem1.py
+++ jsa325/assignment4/problem1.py
@@ -6,7 +6,7 @@ db = MySQLdb.connect(host="localhost", user="jsa325", passwd="M1nd=Body", db="co
 cur = db.cursor()
 
 # create boroughs table
-createCommand1 = "create table if not exists boroughs (zip varcahr(255), borough_name varchar(255))"
+createCommand1 = "create table if not exists boroughs (zip varchar(255), nameBorough varchar(255))"
 cur.execute(createCommand1)
 
 # create zipcode table
@@ -14,14 +14,14 @@ createCommand2 = "create table if not exists zipcodes (zip varchar(255), area de
 cur.execute(createCommand2)
 
 # create incidents table
-createCommand3 = "create table if not exists incidents (zip varcahr(255), address varchar(255), incident_count int)"
+createCommand3 = "create table if not exists incidents (zip varcahr(255), address varchar(255), numIncidents int)"
 cur.execute(createCommand3)
 
 """
 Read data
 """
 
-fileBoroughs = ('boroughs.csv', 'r')
+fileBoroughs = open('boroughs.csv', 'r')
 
 linesBoroughs = []
 for line in fileBoroughs:
@@ -37,6 +37,22 @@ for i in range(0, len(linesBoroughs)):
 # remove duplicates
 removeduplicates = "ALTER IGNORE TABLE boroughs ADD UNIQUE INDEX (zip)"
 cur.execute(removeduplicates)
+
+fileZip = open('zipCodes.csv', 'r')
+
+linesZip = []
+for line in fileZip:
+	linesZip.append(line)
+fileZip.close()
+
+dictZip = {}
+for i in range(1, len(linesZip)):
+	if linesZip[i].split(',')[10]!="\n":
+		dictZip[linesZip[i].split(',')[0]] = (float(linesZip[i].split(',')[10]), float(linesZip[i].split(',')[7]))
+
+for key in dictZip
+	insertCommand2 = "insert into zipcodes values(" + "'" + key "'" _ "," + str(dictZip[key][1]) + "," + srt(dictZip[key][0]) +');"
+	cur.execute(insertCommand2)
 
 db.commit()
 db.close()
