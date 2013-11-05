cb9994185f96ce9df191bec17ad20ca89794ebd7
JSAdams <jsa325@nyu.edu>
2013-11-05 00:46:04 -0500

    Final commit problem2.py

--- jsa325/assignment4/problem2.py
+++ jsa325/assignment4/problem2.py
@@ -10,13 +10,14 @@ db = MySQLdb.connect(host="localhost", user="jsa325", passwd="M1nd=B0dy", db="co
 cur = db.cursor()
 
 # define query to compute population density
-cur.execute("SELECT (population/area) FROM zipcodes WHERE zip = " + "'" + zipGiven + "'" + ";")
+query = "SELECT (population/area) FROM zipcodes WHERE zip = " + "'" + zipGiven + "'" + ";"
+cur.execute(query)
 
 for row in cur.fetchall():
 	if len(cur.fetchall()) > 0:
 		print "The population density in zipcode + 'sys.argv[0]' + " is " str(row[0])."
 	else:
-		print "'sys.argv[0]' + " is not a New York zipcode." 
+		print "'sys.argv[0]' + is not a New York zipcode." 
 
 db.commit()
 db.close()
