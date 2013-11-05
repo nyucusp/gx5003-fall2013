ba739ab6e9f4dec776679d9ea7038f146eb7ea63
JSAdams <jsa325@nyu.edu>
2013-11-04 21:47:47 -0500

    Complete problem5.py

---
+++ jsa325/assignment4/problem5.py
@@ -0,0 +1,13 @@
+import MySQLdb
+
+db = MySQLdb(host="localhost", user="jsa325", passwd="M1nd=B0dy", db="coursedb")
+
+cur = db.cursor()
+
+cur.execute("(SELECT address FROM boroughs JOIN incidents WHERE nameBorough = 'Bronx' AND boroughs.zip = incidents.zip) UNION ALL (SELECT address FROM boroughs JOIN incidents WHERE nameBorough = 'Queens' AND boroughs.zip = incidents.zip);")
+
+for row in cur.fetchall():
+	print row[0]
+
+db.commit()
+db.close
