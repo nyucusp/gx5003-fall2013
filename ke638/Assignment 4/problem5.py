60e2a367922ab55ac5ddf68ca1f9094bd51f2ebd
Katherine Elliott <elliott.katherine.s@gmail.com>
2013-11-08 19:50:46 -0500

    assignment 4

---
+++ ke638/Assignment 4/problem5.py
@@ -0,0 +1,25 @@
+#Katherine Elliott
+#ke638
+#Assignment 4
+#Problem 5
+
+import MySQLdb
+import sys
+
+ #connect to database
+db = MySQLdb.connect(host="127.0.0.1",
+                    user="ke638", # your username
+                    passwd="10plus22", # your password
+                    db="coursedb") # name of the data base
+ 
+ # The Cursor object will let you execute the sql commands
+cur = db.cursor() 
+
+query = "(SELECT incident_address FROM Boroughs_table JOIN Incidents_table WHERE borough = 'Bronx' AND Boroughs_table.zipcode = Incidents_table.incident_zip) \
+UNION ALL (SELECT address FROM Boroughs_table JOIN Incidents_table WHERE borough = 'Queens' AND Boroughs_table.zipcode = Incidents_table.incident_zip);"
+cur.execute(query)
+
+for row in cur.fetchall():
+    print row[0]
+
+db.close()
