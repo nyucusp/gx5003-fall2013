49df05bd5048977c8a69ca63fae1766538dc5016
Haozhe Wang <hw1067@nyu.edu>
2013-11-11 01:59:40 -0500

    fixed sum function

--- hw1067/assignment 4/problem3.py
+++ hw1067/assignment 4/problem3.py
@@ -16,12 +16,12 @@ cur = db.cursor()
 """
 #find population using this query 
 query = "SELECT b.name FROM boroughs AS b "
-query1 = "SELECT SUM(pop_by_zip) FROM zips WHERE  GROUP BY "+ "'" + boroughinput + "'" +"";""      
+query1 = "SELECT SUM(pop_by_zip)  FROM zips WHERE  GROUP BY "+ "'" + boroughinput + "'" +"";""      
 
 query = "SELECT i.num_inc/z.pop_by_zip FROM incident AS i INNER JOIN zips AS z ON z.zip_code = i.zip_code INNER JOIN boroughs as b on z.zip_code = b.zip_code WHERE b.name = "+ "'" + boroughinput + "'" +" GROUP BY "+ "'" + boroughinput + "'" +"" ON 
 """
 
-query_king = "SELECT bp.name, bi.num_inc/bp.pop_by_zip FROM (SELECT b.name name,SUM(num_inc) FROM incident AS i INNER JOIN boroughs AS b ON i.zip_code = b.zip_code WHERE b.name = '"+sys.argv[1]+"' GROUP BY b.name) AS bi INNER JOIN (SELECT b.name name,SUM(pop_by_zip) FROM zips as z INNER JOIN boroughs AS b ON z.zip_code = b.zip_code WHERE b.name = '" + sys.argv[1] +"' GROUP BY b.name) as bp on bi.name = bp.name"
+query_king = "SELECT bp.name, bi.num_inc/bp.pop_by_zip FROM (SELECT b.name name,SUM(num_inc) AS si FROM incident AS i INNER JOIN boroughs AS b ON i.zip_code = b.zip_code WHERE b.name = '"+sys.argv[1]+"' GROUP BY b.name) AS bi INNER JOIN (SELECT b.name name,SUM(pop_by_zip) AS sp FROM zips as z INNER JOIN boroughs AS b ON z.zip_code = b.zip_code WHERE b.name = '" + sys.argv[1] +"' GROUP BY b.name) as bp on bi.name = bp.name"
 #query_king = "SELECT bp.name, bi.num_inc/bp.pop_by_zip FROM (SELECT b.name name,SUM(num_inc) FROM incident AS i INNER JOIN boroughs AS b ON i.zip_code = b.zip_code WHERE b.name = 'Manhattan' GROUP BY b.name) AS bi INNER JOIN (SELECT b.name name,SUM(pop_by_zip) FROM zips as z INNER JOIN boroughs AS b ON z.zip_code = b.zip_code WHERE b.name = 'Manhattan' GROUP BY b.name) as bp on bi.name = bp.name"
 
 #subquery1 = "SELECT SUM(num_inc) FROM incident as i INNER JOIN boroughs AS b ON i.zip_code = b.zip_code WHERE b.name = '"+sys.argv[1]+"' GROUP BY b.name"
