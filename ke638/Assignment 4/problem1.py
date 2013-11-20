669a9bd81be0c6ba56d5484975a5f500af5e7680
Katherine Elliott <elliott.katherine.s@gmail.com>
2013-11-19 20:38:40 -0500

    variable name changes

--- ke638/Assignment 4/problem1.py
+++ ke638/Assignment 4/problem1.py
@@ -27,10 +27,10 @@ cur.execute(dropcommand)
 createCommand = "CREATE table Boroughs_table (zipcode int not null, borough varchar(10), primary key(zipcode))"
 cur.execute(createCommand)
 
-createCommand = "CREATE table ZipCodes_table (name int not null, zip int not null, area int not null, population_perZip int not null, primary key(name))"
+createCommand = "CREATE table ZipCodes_table (name int not null, zip_code int not null, area int not null, population_perZip int not null, primary key(name))"
 cur.execute(createCommand)
 
-createCommand = "CREATE table Incidents_table (incident_address varchar(255), incident_zip int not null, name int not null, population_perZip int not null, primary key(incident_address))"
+createCommand = "CREATE table Incidents_table (incident_address varchar(255), incident_zip int not null, name int not null, primary key(incident_address))"
 cur.execute(createCommand)
 
 open('boroughs.csv')
@@ -50,7 +50,7 @@ for row in zipcodes_csv:
     next(zipcodes_csv)
     if row[10] != ' ':#select only if associated with population
         name = row[0]        
-        zip = row[1]
+        zip_code = row[1]
         area = row[7]
         population_perZip = row[10]
         selectpop = "(SELECT * FROM ZipCodes where zip =" + zip + ");" #eliminate duplicates
@@ -66,7 +66,6 @@ for row in zipcodes_csv:
         incident_address = row[0]        
         incident_zip = row[1]
         name = row[2]
-        population_perZip = row[10]
         insertCommand = "INSERT into Incidents_table values(" + incident_address + "," + "'" + str(incident_zip) + "'" + "," + str(name) + "'" + str(population_perZip) + ");"
         cur.execute(insertCommand)
 
