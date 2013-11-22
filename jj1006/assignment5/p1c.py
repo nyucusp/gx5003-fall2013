becbeabd29518cfcea7ab47d4c5ff5c8d91013ba
justinjee <justinjee@gmail.com>
2013-11-22 17:46:13 -0500

    rest of problem 1

--- jj1006/assignment5/p1c.py
+++ jj1006/assignment5/p1c.py
@@ -15,22 +15,29 @@ for line in finput:
 
 x = [date for (date, apl, msf) in data]
 y = [apl for (date, apl, msf) in data]
-
+z = [msf for (date, apl, msf) in data]
 fig = plt.figure()
 
-graph = fig.add_subplot(111)
-graph.plot_date(x,y,'b-o')
+graph = fig.add_subplot(211)
+graph.plot_date(x,y,'b-o',label='APPLE')
+graph2 = fig.add_subplot(212)
+graph2.plot_date(x,z,'r-o',label='MICROSOFT')
 fig.autofmt_xdate()
 graph.set_xlabel('Month')
 graph.set_ylabel('Apple Stock Price')
+graph2.set_ylabel('Microsoft Stock Price')
 
 graph.spines['top'].set_color('none')
 graph.spines['right'].set_color('none')
 graph.xaxis.set_ticks_position('bottom')
 graph.yaxis.set_ticks_position('left')
 
-graph.annotate('45 degrees for visual emphasis', (x[17],y[17]), xytext=(30,-50), textcoords = 'offset points', arrowprops=dict(facecolor='black', shrink=0.1))
-graph.annotate('Axis labels formatted for readability', (x[-10],40), xytext=(0, 30), textcoords = 'offset points', arrowprops=dict(facecolor='black', shrink=0.1))
+graph2.spines['top'].set_color('none')
+graph2.spines['right'].set_color('none')
+graph2.xaxis.set_ticks_position('bottom')
+graph2.yaxis.set_ticks_position('left')
+
+fig.text(.01,.01,"In this case, it is better to use juxtaposition, since the scale of the Y axis would otherwise \nmake changes in Microsoft's stock price insignificant.")
 
 plt.show()
-fig.savefig('p1a.png')
+fig.savefig('p1c.png')
