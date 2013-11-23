b6d8199a46646a7fdf01b807d4ac41397f54cb4b
AJM777 <AJM777@NYU.edu>
2013-11-23 16:38:00 -0500

    Problem 1A Commit with Buffers

--- ajm777/assignment5/problem1a.py
+++ ajm777/assignment5/problem1a.py
@@ -3,12 +3,7 @@
 #Assignment 5
 #Problem 1a 
 
-
-#import matplotlib.pyplot as plt; plt.rcdefaults()
-#import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as dts
-#import matplotlib.ticker as ticker
 from datetime import date
 
 data = open('stocks.dat','r')
@@ -35,11 +30,21 @@ fig.autofmt_xdate()
 plt.xlabel('Date')
 plt.title("Apple's Stock Quotes at the Start of Each Month, Jan 2006 - Sept 2008")
 plt.ylabel("Stock Quote")
-plt.ylim((0, 220))#Adjusted the y limit so plot had top buffer
+#Adjusted the x&y limits so plot had top & side buffers
+ax.set_ylim(40,210)
+datemin = date(2005, 12, 1)
+datemax = date(2008, 10, 1)
+ax.set_xlim(datemin, datemax)
 plt.grid(which = 'both', color = '0.75', linestyle = '-')#Added light gridlines to make reading easier
 
 
 plt.show()
 
 #____________________Explanation of Plotting Principles
-# With this plot, I decided to use a connected symbol plot because it best represents the data. I followed the principles described in class including clear communication of data and making the data stand out by keeping it neat. I tried to use visual prominence by adding the light grid lines and accurate labels. I also added a buffer to the top of the plot so it would not touch the edge of the plotting rectangle. 
+"""
+With this plot, I decided to use a connected symbol plot because it best represents the 
+data. I followed the principles described in class including clear communication of data 
+and making the data stand out by keeping it neat. I tried to use visual prominence by 
+adding the light grid lines and accurate labels. I also added a buffer to the edges of the
+plot so it would not touch the edge of the plotting rectangle. 
+"""
