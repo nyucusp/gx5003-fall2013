ea05a580dd410e2d5afc932bc0c6ba9e68b7c179
Kara Leary <kll392@nyu.edu>
2013-11-24 11:42:04 -1000

    final problem 1a

--- kll392/assignment5/problem1a.py
+++ kll392/assignment5/problem1a.py
@@ -1,9 +1,10 @@
-import sys
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import pylab
+import matplotlib.ticker as ticker
 
+#Initiate empty lists for entries of month and apple:
 month = []
 apple = []
 
@@ -13,36 +14,44 @@ with open('stocks.dat', 'r') as f:
     for row in rows:
         month.append(row[0])
         apple.append(row[1])
-    
+
+#Reverse month and apple arrays so that they read in chronological order:
 month = month[::-1]
 apple = apple[::-1]
 
-newdates = list(xrange(33))
+#Because every entry in the list of stocks is equally spaced in time, I chose to create a list of simple integers that correspond to every month so that I do not have to deal with datetime objects:
+intDates = list(xrange(len(apple)))
 
+#Instantiate the figure:
 fig, ax = plt.subplots()
-fig.canvas.draw()
 
-ax.plot(newdates, apple, label='AAPL')
+#Plot the list of simple integers with apple stock values
+ax.plot(intDates, apple, label='AAPL')
 
-plt.xticks(np.arange(0, max(newdates) + 1, 1))
+#Specify range for both x and y axes:
+plt.xticks(np.arange(0, max(intDates) + 1, 4))
 plt.yticks(np.arange(40, 240, 20))
 
-xaxis = []
-for i in range(0, len(month)):
-    if (i%7 == 0):
-        xaxis.append(month[i])
+#Formatting the x-axis: set rotation to 30 degrees for a more readable figure:
+xAxis = plt.axes().xaxis
+for tl in xAxis.get_ticklabels():
+    tl.set_rotation(30)
 
+#Fetch a list of the labels on the x axis.  Here I use my month list created earlier to cover the simple integer values with the correct month and year.  I chose to label every fourth tick as it seems to provide the most effective plot:
 labels = [item.get_text() for item in ax.get_xticklabels()]
-for i in range(1, len(month)):
-    if (i%5 == 0):
-        labels[i] = month[i]
-
+for i in range(0, max(intDates)/4 + 1):
+    labels[i] = month[i*4]
 ax.set_xticklabels(labels)
 
+#Properly label the x and y axes, title the graph:
 fig.suptitle('Apple Stock Quotes', fontsize=20)
 plt.xlabel('Month', fontsize = 16)
 plt.ylabel('Stock Value', fontsize = 16)
 
-legend = ax.legend(loc = 'lower right')
+#Place a legend on the graph, which will display the value specified for 'label' in ax.plot():
+legend = ax.legend(loc = 'upper left')
 
+plt.grid(True)
+
+#Finally, show the plot:
 plt.show()
