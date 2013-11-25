e8bbf548222e65bddad95b69cd6e35bbc74c8a55
Christopher Jacoby <cbj238@nyu.edu>
2013-11-25 12:09:18 -0500

    finished problem 2. ish.

--- cbj238/assignment5/problem1.py
+++ cbj238/assignment5/problem1.py
@@ -1,6 +1,6 @@
 """
 author: Christopher B. Jacoby
-date: 2013/19/11
+date: 2013/11/19
 
 For handling dates, I'm using
 http://matplotlib.org/examples/pylab_examples/date_demo1.html
@@ -29,7 +29,7 @@ def problem1a(file_header, file_data):
 
     fig, ax = plt.subplots()
 
-    ax.plot_date(date_vals, apple_stock_vals, '-')
+    ax.plot(date_vals, apple_stock_vals, 'ko-')
 
     # format the ticks
     ax.xaxis.set_major_locator(locator)
@@ -42,7 +42,10 @@ def problem1a(file_header, file_data):
 
     ax.grid(True)
     ax.set_title("Apple Stock Over Time, 2006-01 to 2009-09")
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Stock Price')
 
+    fig.patch.set_facecolor('white')
     fig.autofmt_xdate()
     plt.show()
 
@@ -52,7 +55,36 @@ def problem1b(file_header, file_data):
     by generating a plot using superposition (both curves in the same plot).
     Tag this final plot as "Problem 1b" and annotate it with the conclusions
      you can draw from this plot. """
-    pass
+
+    # Get datetimes from the raw input
+    date_vals = [datetime.strptime(x[0], '%Y-%m') for x in file_data]
+    # get apple stock values from the raw input
+    apple_stock_vals = [x[1] for x in file_data]
+    microsoft_stock_vals = [x[2] for x in file_data]
+
+    fig, ax = plt.subplots(1)
+
+    ax.plot_date(date_vals, apple_stock_vals, '-', label='APPL')
+    ax.plot_date(date_vals, microsoft_stock_vals, '-', label='MSFT')
+
+    # format the ticks
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(yearsFmt)
+    ax.margins(.02, .05)
+    # ax.autoscale_view()
+
+    # Set the ticks to face out (Principle 3)
+    ax.tick_params(direction='out')
+
+    ax.grid(True)
+    ax.set_title("Apple vs. Microsoft Stock, 2006-01 to 2009-09")
+    # grid_bb = ax.get_position()
+    # ax.set_position([grid_bb.x0, grid_bb.y0, grid_bb.width * 0.8, grid_bb.height])
+    # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+
+    fig.patch.set_facecolor('white')
+    fig.autofmt_xdate()
+    plt.show()
 
 def problem1c(file_header, file_data):
     """ (c) Repeat item b, but now using juxtaposition:
@@ -61,7 +93,40 @@ def problem1c(file_header, file_data):
     Tag the final version as "Problem 1c" and annotate it describing
     which technique (superpostion vs. juxtaposition)
     makes more sense for this data and why. """
-    pass
+
+    # Get datetimes from the raw input
+    date_vals = [datetime.strptime(x[0], '%Y-%m') for x in file_data]
+    # get apple stock values from the raw input
+    apple_stock_vals = [x[1] for x in file_data]
+    microsoft_stock_vals = [x[2] for x in file_data]
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+
+    ax1.plot_date(date_vals, apple_stock_vals, '-')
+    ax2.plot_date(date_vals, microsoft_stock_vals, '-')
+
+    # format the ticks
+    ax1.xaxis.set_major_locator(locator)
+    ax1.xaxis.set_major_formatter(yearsFmt)
+    ax1.margins(.02, .05)
+    ax1.autoscale_view()
+    ax2.xaxis.set_major_locator(locator)
+    ax2.xaxis.set_major_formatter(yearsFmt)
+    ax2.margins(.02, .05)
+    ax2.autoscale_view()
+
+    # Set the ticks to face out (Principle 3)
+    ax1.tick_params(direction='out')
+    ax2.tick_params(direction='out')
+
+    ax1.grid(True)
+    ax2.grid(True)
+    ax1.set_title("Apple Stock, 2006-01 to 2009-09")
+    ax2.set_title("Microsoft Stock")
+
+    fig.patch.set_facecolor('white')
+    fig.autofmt_xdate()
+    plt.show()
 
 def main():
     """ main function """
--- cbj238/assignment5/problem2.py
+++ cbj238/assignment5/problem2.py
@@ -4,7 +4,7 @@ date: 2013/11/21
 gx5003 HW5 - Assignment 5 Problem 2
 """
 
-from readdat import read_date_file
+from readdat import ActionFileReader
 import matplotlib.pyplot as plt
 from datetime import datetime
 import numpy as np
@@ -25,36 +25,78 @@ def get_data_range(data):
     last = data[-1]
     return last - first
 
-def get_distances(data):
+def get_dates(fileReader):
+    """ gets the dates from the file as datetime objects. """
+    dates = []
+
+    for date in fileReader:
+        dates.append(date)
+
+    return dates
+
+def get_distances(fileReader):
     """ Computes the time difference between each date and each deadline.
     Returns a numpy array of these, with the rows as each sample and the columns
     as each deadline. """
-    distances = np.zeros([len(data), len(deadlines.keys())], dtype=int)
+    distances = []
 
     # for every entry
-    for index in xrange(len(data)):
-        # compute the time difference, and enter it into the matrix (in seconds.)
+    for date in fileReader:
+        #compute the time difference, and enter it into the matrix (in seconds.)
+        compare_dict = {}
         for deadline in deadlines.keys():
-            distance = (data[index] - deadlines[deadline]).total_seconds()
-            distances[index, int(deadline)] = distance
+            compare_dict[deadline] = deadlines[deadline] - date
+
+        distances.append(compare_dict)
 
     return distances
 
 
 def main():
     """ main function """
-    print "Reading Input."
-    file_data = read_date_file('actions-fall-2007.dat')
-    date_data = [datetime.strptime(x.strip(), '%Y-%m-%d %H:%M:%S') for x in file_data]
+    afr = ActionFileReader()
 
-    date_range = get_data_range(date_data)
-    print "Range:", date_range
+    # Read dates from file
+    dates = sorted(get_dates(afr))
+    dates_float = [ (x - dates[0]).total_seconds() / 3600 / 24. for x in dates]
+    total_days = (dates[-1] - dates[0]).days
 
-    print "Computing distances"
+    fig, ax = plt.subplots()
+    (n, bins, patches) = ax.hist(dates_float, total_days, histtype='stepfilled')
+
+    deadline_float = []
+    for key in sorted(deadlines.keys()):
+        deadline_float.append( (deadlines[key] - dates[0]).total_seconds() / 3600 / 24. )
+    ax.vlines(deadline_float, 0, max(n) * 1.02, 'r')
+
+    ax.set_xticks(deadline_float[1:]) # skip the overlapping one at the beginning.
+    xlabels = [ ("Assignment " + x) for x in sorted(deadlines.keys()[1:]) ]
+    ax.set_xticklabels(xlabels, ha='right', rotation='40')
+
+    ax.margins(.02, 0)
+    ax.grid(True)
+    ax.set_title("Histogram of Actions by Day with Assignment Deadline Overlay.\n")
+
+    ax.set_xlabel('Time (Days), starting at the first action')
+    ax.set_ylabel('Number of actions')
+
+    grid_bb = ax.get_position()
+    ax.set_position([grid_bb.x0, grid_bb.y0 + grid_bb.height * .1, grid_bb.width, grid_bb.height * .9])
+
+    fig.patch.set_facecolor('white')
+    plt.show()
+
+    # print "Reading file and computing distances"
     # Get distances from each deadline to each point.
-    deadline_distances = get_distances(date_data)
-    print deadline_distances[0:10]
+    # deadline_distances = get_distances(afr)
 
+    # int(x['0'].seconds/3600)
+    # for key in deadlines.keys():
+    #     print key, deadlines[key],
+    #     dates_form_deadline = np.array([ x[key].days for x in deadline_distances if x[key].days > -3])
+
+    #     hist = np.histogram(dates_form_deadline)
+    #     print hist
 
 
 if __name__ == "__main__":
