0346c9af83a995ba24c3270e1fc2aec9a0e8a444
Christopher Jacoby <cbj238@nyu.edu>
2013-11-26 13:33:55 -0500

    first part done.

--- cbj238/assignment5/assignment5.js
+++ cbj238/assignment5/assignment5.js
@@ -1,23 +1,26 @@
 var jsonData = new Array(json0, json1, json2, json3, json4, json5, json6, json8, json9, json10, json11, json12, json13, json14, json15, json16, json17, json18, json19);
 var data = getAvailableBikesOverTime();
 var dataLabels = Array();
-for (var i=0; i < data.length; i++) {
-    dataLabels.push(data[i].time);
-}
 
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {top: 20, right: 30, bottom: 100, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
-    .domain(dataLabels)
-    .rangeBands([0, width])
+    .rangeRoundBands([0, width], .1);
 
 var y = d3.scale.linear()
     .range([height, 0])
-    .domain([0, d3.max(data, function(d) {return d.bikes; })]);
+    .domain([d3.min(data, function(d) {return d.bikes; }) *.995, d3.max(data, function(d) {return d.bikes; })]);
 
-var barWidth = width / data.length;
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
 
 function getAvailableBikes(jsonObject) {
     var total = 0;
@@ -36,26 +39,43 @@ function getAvailableBikesOverTime() {
 }
 
 window.onload=function() {
+    x.domain(data.map(function(d) { return d.time; }));
+
     var bikeChart = d3.select("#chart1")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
     .append("g")
-        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+        .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
 
-    var bar = bikeChart.selectAll("g")
+    bikeChart.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate (-65)"
+            });
+
+    bikeChart.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+        .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Available Bikes");
+
+    var bar = bikeChart.selectAll(".bar")
         .data(data)
-    .enter().append("g")
-        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
-
-    bar.append("rect")
+    .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.time); })
         .attr("y", function(d) { return y(d.bikes); })
         .attr("height", function(d) { return height - y(d.bikes); })
-        .attr("width", barWidth - 1);
-
-    bar.append("text")
-        .attr("x", barWidth / 2)
-        .attr("y", function(d) { return y(d.bikes) + 3; })
-        .attr("dy", ".75em")
-        .text(function(d) { return d.bikes; });
+        .attr("width", x.rangeBand());
 }
 
