538e5cf453989bac53af9a0628a71728f61359ad
Richard Allen Dunks <richarddunks@gmail.com>
2013-11-27 18:41:02 -0500

    updated x labels in problem 3

--- rad416/Assignment5/problem3.py
+++ rad416/Assignment5/problem3.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import math
 from _collections import defaultdict
 
+"""
+A script to take in the list of processors with their year of introduction and the number of
+transistors.  Two dot plots are created to display the information.
+"""
+
 mpFile = open('microprocessors.dat','r')
 next(mpFile) # dump header file
 
@@ -31,15 +36,16 @@ processorRankList = []
 for k in sorted(processorDict.items(), key=lambda x: x[1]):
     processorRankList.append( k[0] )
 
-
 fig = plt.figure(figsize = (25,10))
 ax1 = fig.add_subplot(1,2,1)
 ax1.plot(sorted_years, rangeList, 'o')
 ax1.grid(True,which='both')
+ax1.set_xlabel("Year of Introduction")
 plt.yticks(rangeList, processorRankList)
 ax2 = fig.add_subplot(122)
 ax2.plot(transLog10_sort, rangeList, 'o')
 ax2.grid(True,which='both')
+ax2.set_xlabel("Number of Transistors (log10)")
 ax1.tick_params(axis='both', direction='out', labelsize=10)
 ax2.tick_params(axis='both', direction='out', labelsize=10)
 plt.yticks(rangeList, processorRankList)
