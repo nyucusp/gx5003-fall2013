7193f990e909c9d465cd799478b84c1915113f28
JSAdams <jsa325@nyu.edu>
2013-12-09 12:55:00 -0500

    Initial commit for part b: initialize target and training sets, compute RMSE, RSQ, and residuals

--- jsa325/Assignment 6/problem6.py
+++ jsa325/Assignment 6/problem6.py
@@ -114,6 +114,64 @@ plt.clf()
 Part b: cross-validation, select model complexity based on RMSE and OLS scores for 1–5th order polynomial models
 
 """
+
+# Initialize target and training sets
+
+def kfolds(numberFolds, listTrain, listTest):
+    numberFolds = numberFolds
+    sizeCrossValidation = len(listTrain)/numberFolds
+    
+    arrayTarget = []
+    arrayTrain = []
+    
+    for x in range(numberFolds):
+        data = zip(listTest, listTrain)
+        boundLower = sizeCrossValidation * x
+        boundUpper = sizeCrossValidation * (x + 1)
+        
+        setTarget = data[boundLower:boundUpper]
+        arrayTarget.append(setTarget)
+        
+        setTrain = data[:boundLower]
+        setTrain.extend(data[boundUpper])
+        arrayTrain.append(setTrain)
+        
+    return numberFolds, sizeCrossValidation, arrayValidate, arrayTrain
+
+# Define functions to calculate RMSE and RSQ scores
+
+def calculateRMSE(x, y, xTarget=None, yTarget=None, degree=0, computeRSQ=True)
+
+    if xTarget==None:
+        xTarget = x
+        yTarget = y
+
+    # Fit to training set
+
+    coefFit = np.polyfit(x, y, degree)    
+    
+    # Compute residuals on validation set
+
+    residualsRSQ = sum((yTarget - np.polyval(coefFit, xTarget))**2)
+
+    # Compute RMSE and RSQ scores from validation set on training regression
+
+    RMSE = residualsRSQ/len(xTarget)
+    RSQ = RMSE**(0.5)
+
+    # Define function to calculate RMSE scores for 1–5th order polynomial models
+
+    if computeRSQ:
+        averageTarget = sum(yTarget/len(yTarget))
+        for yi in yTarget:
+            listSquares = (yi - averageTarget)**2
+        sumSquares = sum(listSquares)
+        RSQ = 1 - (residualsRSQ/sumSquares)
+    else:
+        RSQ = 0
+
+    # Compute RMSE and RSQ scores for models
+
 """
 Part c: plot RMSE of whole training set against 10-fold CV average
 
