550fb62e173d9d0da3863b626c339dd46b5e7fb9
lz1023 <lz1023@nyu.edu>
2013-12-18 22:13:53 -0500

    add cvs file

--- lz1023/assignment6/problemd.py
+++ lz1023/assignment6/problemd.py
@@ -21,8 +21,10 @@ myfile = open('unlabeled_data.csv','r')
 for line in myfile:
     readdata1.append(line)
 pop1=[]
+zip1=[]
 for i in range(1,len(readdata1)):
     pop1.append(float(readdata1[i].split(",")[1]))
+    zip1.append(float(readdata1[i].split(",")[0]))
 myfile.close()
 
 # using degree 3 polynomial as the model
@@ -30,6 +32,12 @@ coeffs=np.polyfit(pop,inc,3)
 p=np.poly1d(coeffs)
 inc_predict=p(pop1)
 
+#print inc_predict
+outputfile=open('prediction.cvs','w')
+outputfile.write('zipcode'+','+'Population'+','+'Predicted incidents'+'\n')
+for i in range(0,len(pop1)):
+    outputfile.write(str(zip1[i])+','+str(pop1[i])+','+str(inc_predict[i])+'\n')
+outputfile.close()
 
 chart=plt.figure()
 chart=plt.subplot(1,1,1)
