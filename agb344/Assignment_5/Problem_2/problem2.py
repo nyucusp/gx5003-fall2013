792e7680f633d4e4939ef26b625ae850acbac14c
amirbeshay <amirbeshay@gmail.com>
2013-12-21 22:29:49 -0500

    updated prob 2

--- agb344/Assignment_5/Problem_2/problem2.py
+++ agb344/Assignment_5/Problem_2/problem2.py
@@ -17,20 +17,5 @@ print hist, edges
 plt.hist(dateTimeList)
 
 fig = pyplot.figure()
-ax = fig.add_subplot(111)
-ax.bar(edges[:-1], hist / width, width=width)
-ax.set_xlim(bin_edges[0], num_now())
-ax.set_ylabel('Events [1/day]')
-if title:
-    ax.set_title(title)
- 
-    # set x-ticks in date
-    # see: http://matplotlib.sourceforge.net/examples/api/date_demo.html
-ax.xaxis.set_major_locator(YearLocator())
-ax.xaxis.set_major_formatter(DateFormatter('%Y'))
-ax.xaxis.set_minor_locator(MonthLocator())
-    # format the coords message box
-ax.format_xdata = DateFormatter('%Y-%m-%d')
-ax.grid(True)
- 
+ax = fig.add_subplot(111) 
 fig.autofmt_xdate()
